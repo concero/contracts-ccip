2985128aac7828bde806af90df5b5ece4e52f3aa
Oleg Kirillovich <olegkron@ya.ru>
2024-04-04 01:32:59 +0100

    CL functions src code

--- packages/hardhat/contracts/CLFunctionsSRC.js
+++ packages/hardhat/contracts/CLFunctionsSRC.js
@@ -1,18 +1,18 @@
 const chainSelectors = {
-  12532609583862916517: {
+  '12532609583862916517': {
     id: 80001,
     url: "https://polygon-mumbai.infura.io/v3/",
-    conceroCCIP: "0x4b1f1e2435a9c96f7330faea190ef6e7b0b1b1a7",
+    conceroCCIP: "0xfddaffa49e71da3ef0419a303a6888f94bb5ba18",
   },
-  14767482510784806043: {
+  '14767482510784806043': {
     id: 43113,
     url: "https://avalanche-fuji.infura.io/v3/",
-    conceroCCIP: "0x4b1f1e2435a9c96f7330faea190ef6e7b0b1b1a7",
+    conceroCCIP: "0xfddaffa49e71da3ef0419a303a6888f94bb5ba18",
   },
 };
 
 const topics = ["0x74bbc026808dcba59692d6a8bb20596849ca718e10e2432c6cdf48af865bc5d9"];
-const [fromChainSelector, toChainSelector, token, amount, none, txHash, sender, receiver, blockHash] = args;
+const [fromChainSelector, toChainSelector, token, amount, txHash, sender, receiver, blockHash] = args;
 
 let chain = chainSelectors[toChainSelector];
 if (chain) {
@@ -37,12 +37,30 @@ if (chain) {
       id: 1,
     },
   });
+
+
   const dstContractRes = await dstContractReq;
-  if (dstContractRes.error) throw new Error("Error fetching destination contract address");
-  if (!dstContractRes.result) throw new Error("Result is undefined");
+  console.log(JSON.stringify(dstContractRes))
+  if (dstContractRes.error) {
+    console.error(dstContractRes.error);
+    throw new Error("Error fetching destination contract address");
+  }
+  if (!dstContractRes.result) {
+    console.error(dstContractRes);
+    throw new Error("Result is undefined");
+  }
   const filtered = dstContractRes.result.filter(log => log.transactionHash === txHash);
-  if (filtered.length === 0) throw new Error("No logs found for txHash");
-  if (filtered.length > 1) throw new Error("Multiple logs found for txHash");
-  if (filtered[0].data === "0x") throw new Error("Data is empty");
-  return filtered[0].data;
+  if (filtered.length === 0) {
+    console.error(dstContractRes);
+    throw new Error("No logs found for txHash");
+  }
+  if (filtered.length > 1) {
+    console.error(dstContractRes);
+    throw new Error("Multiple logs found for txHash");
+  }
+  if (filtered[0].data === "0x") {
+    console.error(dstContractRes);
+    throw new Error("Data is empty");
+  }
+  return Functions.encodeString('test')
 }

b10fe58f0389e96b04f2007a3137b7abfeb3945e
Oleg Kirillovich <olegkron@ya.ru>
2024-04-07 20:02:43 +0100

    working SRC functions

--- CLFunctions/CLF_src/CLFsrc-CUSTOM-TRANSPORT-FUNCTIONS-SIMULATION.js
+++ CLFunctions/CLF_src/CLFsrc-CUSTOM-TRANSPORT-FUNCTIONS-SIMULATION.js
@@ -4,11 +4,11 @@ numAllowedQueries: 2 – a minimum to initialise Viem.
  */
 
 // CUSTOM TRANSPORT FUNCTIONS
-// import { Functions } from './sandbox.ts';
-//
+// import { Functions } from '../utils/sandbox.ts';
+
 // const secrets = {
-//   WALLET_PRIVATE_KEY: '44c04f3751b5e35344400ab7f7e561c3b80c02c2f87de69a561ecbf6d0018896',
-//   INFURA_API_KEY: '8acf47c71165427f8cee3a92fea12da2',
+//  WALLET_PRIVATE_KEY: '44c04f3751b5e35344400ab7f7e561c3b80c02c2f87de69a561ecbf6d0018896',
+//  INFURA_API_KEY: '8acf47c71165427f8cee3a92fea12da2',
 // };
 // const args = ["12532609583862916517"];
 // const chainSelectors = {
@@ -18,47 +18,61 @@ numAllowedQueries: 2 – a minimum to initialise Viem.
 
 // async function sendTx() {
 // const url = `https://polygon-mumbai.infura.io/v3/${secrets.INFURA_API_KEY}`;
+
 const url = `https://polygon-mumbai.gateway.tenderly.co`;
 const { createWalletClient, custom } = await import('npm:viem');
 const { privateKeyToAccount } = await import('npm:viem/accounts');
 const { polygonMumbai } = await import('npm:viem/chains');
 
 try {
-  const client = createWalletClient({
-    chain: polygonMumbai,
-    transport: custom({
-      async request({ method, params }) {
-        // Hardcoded responses
-        if (method === 'eth_chainId') return '0x13881';
-        if (method === 'eth_estimateGas') return '0x5208';
-        if (method === 'eth_maxPriorityFeePerGas') return '0x3b9aca00';
-
-        const req = {
-          url,
-          method: 'post',
-          headers: { 'Content-Type': 'application/json' },
-          data: { jsonrpc: '2.0', id: 1, method, params },
-        };
-
-        console.log('REQUEST: ', JSON.stringify(req));
-        const response = await Functions.makeHttpRequest(req);
-        console.log('Response: ', response.data.result);
-        if (response.error || !response.data.result) throw new Error('error');
-        return response.data.result;
-      },
-    }),
-  });
-  const account = privateKeyToAccount('0x' + secrets.WALLET_PRIVATE_KEY);
-  // console.log('ACCOUNT: ', account);
-  // return Functions.encodeString(account.address);
-  const hash = await client.sendTransaction({
-    account,
-    to: '0x70E73f067a1fC9FE6D53151bd271715811746d3a',
-    value: 1000000n,
-    chain: polygonMumbai,
-  });
-  // console.log(`Transaction: ${hash}`);
-  return Functions.encodeString(hash);
+ const client = createWalletClient({
+  chain: polygonMumbai,
+  transport: custom({
+   async request({ method, params }) {
+    if (method === 'eth_chainId') return '0x13881';
+    if (method === 'eth_estimateGas') return '0x3d090';
+    if (method === 'eth_maxPriorityFeePerGas') return '0x3b9aca00';
+    const response = await Functions.makeHttpRequest({
+     url,
+     method: 'post',
+     headers: { 'Content-Type': 'application/json' },
+     data: { jsonrpc: '2.0', id: 1, method, params },
+    });
+    return response.data.result;
+   },
+  }),
+ });
+ const account = privateKeyToAccount('0x' + secrets.WALLET_PRIVATE_KEY);
+ const abi = [
+  {
+   name: 'addUnconfirmedTX',
+   type: 'function',
+   inputs: [
+    { type: 'bytes32', name: 'ccipMessageId' },
+    { type: 'address', name: 'sender' },
+    { type: 'address', name: 'recipient' },
+    { type: 'uint256', name: 'amount' },
+    { type: 'uint64', name: 'srcChainSelector' },
+    { type: 'address', name: 'token' },
+   ],
+   outputs: [],
+  },
+ ];
+ const hash = await client.writeContract({
+  account,
+  abi,
+  address: '0x59d607709841174d20aAFc5e8A1357C4940e8e9F',
+  args: [
+   '0x8b666e9ea0f849048bfb59996e02f0082df9298550249d7c6cefec78e7e24cd8',
+   '0x70E73f067a1fC9FE6D53151bd271715811746d3a',
+   '0x70E73f067a1fC9FE6D53151bd271715811746d3a',
+   '1000000000000000000',
+   parseInt('12532609583862916517'),
+   '0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97',
+  ],
+  chain: polygonMumbai,
+ });
+ return Functions.encodeString(hash);
 } catch (error) {
-  return Functions.encodeString('error');
+ return Functions.encodeString('error');
 }
--- packages/hardhat/contracts/ConceroBridge.sol
+++ packages/hardhat/contracts/ConceroBridge.sol
@@ -1,93 +1,93 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <=0.8.19;
+pragma solidity ^0.8.19;
 
-import { ConceroCCIP } from "./ConceroCCIP.sol";
-import { ConceroFunctions } from "./ConceroFunctions.sol";
-import { IERC20 } from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import {ConceroCCIP} from "./ConceroCCIP.sol";
+import {ConceroFunctions} from "./ConceroFunctions.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 
 contract ConceroBridge is ConceroCCIP, ConceroFunctions {
-	string srcChainRequestSourceCode = "console.log('test')";
+    string srcChainRequestSourceCode = "console.log('test')";
 
-	constructor(
-		address _link,
-		address _ccipRouter,
-		address _functionsRouter,
-		bytes32 _donId
-	)
-		ConceroCCIP(_link, _ccipRouter)
-		ConceroFunctions(_functionsRouter, _donId)
-	{}
+    constructor(
+        address _link,
+        address _ccipRouter,
+        address _functionsRouter,
+        bytes32 _donId
+    )
+    ConceroCCIP(_link, _ccipRouter)
+    ConceroFunctions(_functionsRouter, _donId)
+    {}
 
-	receive() external payable {}
+    receive() external payable {}
 
-	function allowDestinationChain(
-		uint64 _dstChainSelector,
-		bool allowed
-	) external onlyOwner {
-		allowListedDstChains[_dstChainSelector] = allowed;
-	}
+    function allowDestinationChain(
+        uint64 _dstChainSelector,
+        bool allowed
+    ) external onlyOwner {
+        allowListedDstChains[_dstChainSelector] = allowed;
+    }
 
-	function allowSourceChain(
-		uint64 _srcChainSelector,
-		bool allowed
-	) external onlyOwner {
-		allowListedSrcChains[_srcChainSelector] = allowed;
-	}
+    function allowSourceChain(
+        uint64 _srcChainSelector,
+        bool allowed
+    ) external onlyOwner {
+        allowListedSrcChains[_srcChainSelector] = allowed;
+    }
 
-	function allowListSender(address _sender, bool allowed) external onlyOwner {
-		allowListedSenders[_sender] = allowed;
-	}
+    function allowListSender(address _sender, bool allowed) external onlyOwner {
+        allowListedSenders[_sender] = allowed;
+    }
 
-	function startTransaction(
-		address _token,
-		uint256 _amount,
-		uint64 _destinationChainSelector,
-		address _receiver
-	) external payable tokenAmountSufficiency(_token, _amount) {
-		bool isOK = IERC20(_token).transferFrom(
-			msg.sender,
-			address(this),
-			_amount
-		);
+    function startTransaction(
+        address _token,
+        uint256 _amount,
+        uint64 _destinationChainSelector,
+        address _receiver
+    ) external payable tokenAmountSufficiency(_token, _amount) {
+        bool isOK = IERC20(_token).transferFrom(
+            msg.sender,
+            address(this),
+            _amount
+        );
 
-		require(isOK, "Transfer failed");
+        require(isOK, "Transfer failed");
 
-		_sendTokenPayLink(
-			_destinationChainSelector,
-			_receiver,
-			_token,
-			_amount
-		);
-	}
+        _sendTokenPayLink(
+            _destinationChainSelector,
+            _receiver,
+            _token,
+            _amount
+        );
+    }
 
-	function withdraw(address _owner) public onlyOwner {
-		uint256 amount = address(this).balance;
+    function withdraw(address _owner) public onlyOwner {
+        uint256 amount = address(this).balance;
 
-		if (amount == 0) {
-			revert NothingToWithdraw();
-		}
+        if (amount == 0) {
+            revert NothingToWithdraw();
+        }
 
-		(bool sent, ) = _owner.call{ value: amount }("");
+        (bool sent,) = _owner.call{value: amount}("");
 
-		if (!sent) {
-			revert FailedToWithdrawEth(msg.sender, _owner, amount);
-		}
-	}
+        if (!sent) {
+            revert FailedToWithdrawEth(msg.sender, _owner, amount);
+        }
+    }
 
-	function withdrawToken(address _owner, address _token) public onlyOwner {
-		uint256 amount = IERC20(_token).balanceOf(address(this));
+    function withdrawToken(address _owner, address _token) public onlyOwner {
+        uint256 amount = IERC20(_token).balanceOf(address(this));
 
-		if (amount == 0) {
-			revert NothingToWithdraw();
-		}
+        if (amount == 0) {
+            revert NothingToWithdraw();
+        }
 
-		IERC20(_token).transfer(_owner, amount);
-	}
+        IERC20(_token).transfer(_owner, amount);
+    }
 
-	function handleTransaction(
-		string[] calldata args,
-		uint64 subscriptionId
-	) external onlyOwner {
-		sendRequest(srcChainRequestSourceCode, subscriptionId, args);
-	}
+    function handleTransaction(
+        string[] calldata args,
+        uint64 subscriptionId
+    ) external onlyOwner {
+        sendRequest(srcChainRequestSourceCode, subscriptionId, args);
+    }
 }

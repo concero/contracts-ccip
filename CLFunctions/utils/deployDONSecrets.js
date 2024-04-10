f82233cd7ccc95f4b47d47d480dec11b8fea5c83
lufaque <95128636+lufaque@users.noreply.github.com>
2024-04-10 23:00:25 +0300

    fix: deploy script

--- CLFunctions/utils/deployDONSecrets.js
+++ CLFunctions/utils/deployDONSecrets.js
@@ -4,19 +4,27 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: '../../.env' });
 
+// mumbai
 const mumbaiRpcUrl = `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`;
 const mumbaiDonId = 'fun-polygon-mumbai-1';
 const mumbaiFunctionsRouterAddress = '0x6E2dc0F9DB014aE19888F539E59285D2Ea04244C';
+
+// fuji
+const fujiRpcUrl = `https://avalanche-fuji.infura.io/v3/${process.env.INFURA_API_KEY}`;
+const fujiDonId = 'fun-avalanche-fuji-1';
+const fujiFunctionsRouterAddress = process.env.CL_FUNCTIONS_ROUTER_FUJI;
+
+// common
 const privateKey = process.env.DEPLOYER_PRIVATE_KEY;
 const slotIdNumber = 0;
 const expirationTimeMinutes = 4320;
 
-const main = async () => {
+const deploySecrets = async (functionsRouterAddress, donId, rpcUrl) => {
  const secrets = {
   INFURA_API_KEY: process.env.INFURA_API_KEY,
   WALLET_PRIVATE_KEY: process.env.SECOND_TEST_WALLET_PRIVATE_KEY,
  };
- const provider = new ethers.providers.JsonRpcProvider(mumbaiRpcUrl);
+ const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
  const wallet = new ethers.Wallet(privateKey);
  const signer = wallet.connect(provider);
  const gatewayUrls = [
@@ -26,8 +34,8 @@ const main = async () => {
 
  const secretsManager = new SecretsManager({
   signer: signer,
-  functionsRouterAddress: mumbaiFunctionsRouterAddress,
-  donId: mumbaiDonId,
+  functionsRouterAddress: functionsRouterAddress,
+  donId: donId,
  });
 
  await secretsManager.initialize();
@@ -51,6 +59,10 @@ const main = async () => {
  console.log(JSON.stringify(secretsEntriesForGateway, null, 2));
 };
 
-main().catch(err => {
+// deploySecrets(mumbaiFunctionsRouterAddress, mumbaiDonId, mumbaiRpcUrl).catch(err => {
+//  console.log('ERROR: ', JSON.stringify(err, null, 2));
+// });
+
+deploySecrets(fujiFunctionsRouterAddress, fujiDonId, fujiRpcUrl).catch(err => {
  console.log('ERROR: ', JSON.stringify(err, null, 2));
 });

6d62979d3d11e0c2cd07da312928648976e85376
lufaque <95128636+lufaque@users.noreply.github.com>
2024-04-10 20:43:30 +0300

    fix: deploy script

--- packages/hardhat/deploy/00_CFunctions.ts
+++ packages/hardhat/deploy/00_CFunctions.ts
@@ -20,7 +20,7 @@ const deployCFunctions: DeployFunction = async function (hre: HardhatRuntimeEnvi
       CL_FUNCTIONS_ROUTER_MUMBAI,
       CL_FUNCTIONS_DON_ID_MUMBAI,
       1437,
-      1712503865,
+      1712770854,
       "0x4200A2257C399C1223f8F3122971eb6fafaaA976",
       "0x3A684e72D220Ce842354bebf9AfFCdA34EE27D82",
       "12532609583862916517",

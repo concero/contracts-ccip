af63f5375be52c00592f8fbdb578ab2926702bba
Oleg Kirillovich <olegkron@ya.ru>
2024-04-15 01:56:30 +0100

    upload don secrets task

--- packages/hardhat/tasks/Functions-consumer/uploadSecretsToDon.ts
+++ packages/hardhat/tasks/Functions-consumer/uploadSecretsToDon.ts
@@ -1,9 +1,11 @@
 const { SecretsManager } = require("@chainlink/functions-toolkit");
-import { networks } from "../../constants/CLFnetworks";
+import networks from "../../constants/CLFnetworks";
 
 const process = require("process");
-const path = require("path");
+// const path = require("path");
+// run with: bunx hardhat functions-upload-secrets-don --slotid 0 --ttl 4320 --network avalancheFuji
 
+// todo: add to deployedSecrets file with expiration time, and check if it's expired before using itV
 task("functions-upload-secrets-don", "Encrypts secrets and uploads them to the DON")
   .addParam("slotid", "Storage slot number 0 or higher - if the slotid is already in use, the existing secrets for that slotid will be overwritten")
   .addOptionalParam("ttl", "Time to live - minutes until the secrets hosted on the DON expire (defaults to 10, and must be at least 5)", 10, types.int)
@@ -25,16 +27,21 @@ task("functions-upload-secrets-don", "Encrypts secrets and uploads them to the D
     });
     await secretsManager.initialize();
 
-    // Get the secrets object from  Functions-request-config.js or other specific request config.
-    const requestConfig = require(path.isAbsolute(taskArgs.configpath) ? taskArgs.configpath : path.join(process.cwd(), taskArgs.configpath));
+    // // Get the secrets object from  Functions-request-config.js or other specific request config.
+    // const requestConfig = require(path.isAbsolute(taskArgs.configpath) ? taskArgs.configpath : path.join(process.cwd(), taskArgs.configpath));
+    //
+    // if (!requestConfig.secrets || requestConfig.secrets.length === 0) {
+    //   console.log("No secrets found in the request config.");
+    //   return;
+    // }
 
-    if (!requestConfig.secrets || requestConfig.secrets.length === 0) {
-      console.log("No secrets found in the request config.");
-      return;
-    }
+    const secrets = {
+      INFURA_API_KEY: process.env.INFURA_API_KEY,
+      WALLET_PRIVATE_KEY: process.env.SECOND_TEST_WALLET_PRIVATE_KEY,
+    };
 
     console.log("Encrypting secrets and uploading to DON...");
-    const encryptedSecretsObj = await secretsManager.encryptSecrets(requestConfig.secrets);
+    const encryptedSecretsObj = await secretsManager.encryptSecrets(secrets);
 
     const {
       version, // Secrets version number (corresponds to timestamp when encrypted secrets were uploaded to DON)

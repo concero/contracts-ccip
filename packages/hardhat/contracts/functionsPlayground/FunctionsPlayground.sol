e8b11a6b10804da051ab176216e1ed0e6e25de5b
lufaque <95128636+lufaque@users.noreply.github.com>
2024-05-06 18:34:03 +0300

    feat: add secrets to FunctionsPlayground.sol

--- packages/hardhat/contracts/functionsPlayground/FunctionsPlayground.sol
+++ packages/hardhat/contracts/functionsPlayground/FunctionsPlayground.sol
@@ -11,28 +11,47 @@ contract FunctionsPlayground is FunctionsClient, ConfirmedOwner {
   bytes32 public s_lastRequestId;
   bytes public s_lastResponse;
   bytes public s_lastError;
+  uint64 public donHostedSecretsVersion;
+  uint8 public donHostedSecretsSlotID;
 
   error UnexpectedRequestID(bytes32 requestId);
 
   event Response(bytes32 indexed requestId, string character, bytes response, bytes err);
 
-  bytes32 immutable donID;
+  bytes32 public immutable donID;
 
   string public character;
   uint64 public subscriptionId;
 
-  constructor(address _router, bytes32 _donId, uint64 _subscriptionId) FunctionsClient(_router) ConfirmedOwner(msg.sender) {
+  constructor(
+    address _router,
+    bytes32 _donId,
+    uint64 _subscriptionId,
+    uint64 _donHostedSecretsVersion,
+    uint8 _donHostedSecretsSlotID
+  ) FunctionsClient(_router) ConfirmedOwner(msg.sender) {
     donID = _donId;
     subscriptionId = _subscriptionId;
+    donHostedSecretsVersion = _donHostedSecretsVersion;
+    donHostedSecretsSlotID = _donHostedSecretsSlotID;
   }
 
   function setSubscriptionId(uint64 _subscriptionId) external onlyOwner {
     subscriptionId = _subscriptionId;
   }
 
+  function setDonHostedSecretsVersion(uint64 _version) external onlyOwner {
+    donHostedSecretsVersion = _version;
+  }
+
+  function setDonHostedSecretsSlotID(uint8 _slotID) external onlyOwner {
+    donHostedSecretsSlotID = _slotID;
+  }
+
   function sendRequest(string calldata sourceCode, string[] calldata args) external onlyOwner returns (bytes32 requestId) {
     FunctionsRequest.Request memory req;
     req.initializeRequestForInlineJavaScript(sourceCode);
+    req.addDONHostedSecrets(donHostedSecretsSlotID, donHostedSecretsVersion);
     if (args.length > 0) req.setArgs(args);
 
     s_lastRequestId = _sendRequest(req.encodeCBOR(), subscriptionId, 300000, donID);

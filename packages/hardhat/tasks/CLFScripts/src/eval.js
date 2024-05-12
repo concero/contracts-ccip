5c2a7c55a0994911ebee9928e76903cbe1ca1835
Oleg Kirillovich <olegkron@ya.ru>
2024-05-12 13:31:48 +0100

    clf secrets js code

--- packages/hardhat/tasks/CLFScripts/src/eval.js
+++ packages/hardhat/tasks/CLFScripts/src/eval.js
@@ -1,8 +1,2 @@
-try {
-	const ethers = await import('npm:ethers@6.10.0');
-	const res = eval(secrets.test);
-	return res;
-} catch (error) {
-	console.log(error);
-	return Functions.encodeString(error);
-}
+await import('npm:ethers@6.10.0');
+return eval(secrets.srcjs);

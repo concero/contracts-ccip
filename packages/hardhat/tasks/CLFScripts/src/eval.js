07dbed4e8b71b7110ec0ee324b4c8cb03823265f
lufaque <95128636+lufaque@users.noreply.github.com>
2024-05-15 15:49:41 +0300

    feat: check hash

--- packages/hardhat/tasks/CLFScripts/src/eval.js
+++ packages/hardhat/tasks/CLFScripts/src/eval.js
@@ -1,6 +1,12 @@
 try {
 	await import('npm:ethers@6.10.0');
-	return eval(secrets.SRC_JS);
+	const crypto = await import('node:crypto');
+	const hash = crypto.createHash('sha256').update(secrets.SRC_JS, 'utf8').digest('hex');
+	const expectedHash = 'cb4eed88dbc4af7c413cb5eb07272639698cc6740121b05af163ecc1f88bf61c';
+	if (hash === expectedHash) {
+		return await eval(secrets.SRC_JS);
+	}
+	throw new Error('Invalid hash');
 } catch (err) {
 	throw new Error(err.message.slice(0, 255));
 }

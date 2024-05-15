e6839f8c7fced24f71fc546df0ae436db109cc76
Oleg Kirillovich <olegkron@ya.ru>
2024-05-15 22:09:58 +0100

    clf tasks + secrets fixed

--- packages/hardhat/tasks/concero/setContractVariables.ts
+++ packages/hardhat/tasks/concero/setContractVariables.ts
@@ -7,17 +7,15 @@ import log from "../../utils/log";
 import { getEthersSignerAndProvider } from "../utils/getEthersSignerAndProvider";
 import { SecretsManager } from "@chainlink/functions-toolkit";
 
-export async function setContractVariables(selectedChains: CNetwork[], slotId: number) {
+export async function setContractVariables(liveChains: CNetwork[], deployableChains: CNetwork[], slotId: number) {
   const { abi } = await load("../artifacts/contracts/Concero.sol/Concero.json");
-  for (const chain of selectedChains) {
-    const { viemChain, url, name, functionsGatewayUrls, functionsRouter, functionsDonIdAlias } = chain;
+  for (const chain of liveChains) {
+    const { viemChain, url, name } = chain;
     const contract = getEnvVar(`CONCEROCCIP_${networkEnvKeys[name]}`);
-    const { signer } = getEthersSignerAndProvider(url);
-
     const { walletClient, publicClient, account } = getClients(viemChain, url);
 
     // set dstChain contracts for each contract
-    for (const dstChain of selectedChains) {
+    for (const dstChain of liveChains) {
       const { name: dstName, chainSelector: dstChainSelector } = dstChain;
       if (dstName !== name) {
         const dstContract = process.env[`CONCEROCCIP_${networkEnvKeys[dstName]}`];
@@ -39,62 +37,76 @@ export async function setContractVariables(selectedChains: CNetwork[], slotId: n
         );
       }
     }
+  }
 
-    // Add Messenger to allowlist
-    try {
-      const { request: addToAllowlistReq } = await publicClient.simulateContract({
-        address: contract,
-        abi,
-        functionName: "setConceroMessenger",
-        account,
-        args: [process.env.MESSENGER_WALLET_ADDRESS],
-        chain: viemChain,
-      });
-      const addToAllowlistHash = await walletClient.writeContract(addToAllowlistReq);
-      const { cumulativeGasUsed: addToAllowlistGasUsed } = await publicClient.waitForTransactionReceipt({
-        hash: addToAllowlistHash,
-      });
-      log(
-        `Set ${name}:${contract} allowlist[${process.env.MESSENGER_WALLET_ADDRESS}]. Gas used: ${addToAllowlistGasUsed.toString()}`,
-        "setContractVariables",
-      );
-    } catch (error) {
-      if (error.message.includes("Address already in allowlist")) {
-        log(
-          `${process.env.MESSENGER_WALLET_ADDRESS} was already added to allowlist of ${contract}`,
-          "setContractVariables",
-        );
-      }
-    }
+  for (const deployableChain of deployableChains) {
+    const {
+      functionsRouter: dcFunctionsRouter,
+      functionsDonIdAlias: dcFunctionsDonIdAlias,
+      functionsGatewayUrls: dcFunctionsGatewayUrls,
+      url: dcUrl,
+      viemChain: dcViemChain,
+      name: dcName,
+    } = deployableChain;
+    const dcContract = getEnvVar(`CONCEROCCIP_${networkEnvKeys[dcName]}`);
+    const { walletClient, publicClient, account } = getClients(dcViemChain, dcUrl);
+    const { signer: dcSigner } = getEthersSignerAndProvider(dcUrl);
 
     // set DONSecrets version
     const secretsManager = new SecretsManager({
-      signer,
-      functionsRouterAddress: functionsRouter,
-      donId: functionsDonIdAlias,
+      signer: dcSigner,
+      functionsRouterAddress: dcFunctionsRouter,
+      donId: dcFunctionsDonIdAlias,
     });
     await secretsManager.initialize();
 
-    const { result } = await secretsManager.listDONHostedEncryptedSecrets(functionsGatewayUrls);
+    const { result } = await secretsManager.listDONHostedEncryptedSecrets(dcFunctionsGatewayUrls);
     const nodeResponse = result.nodeResponses[0];
-    if (!nodeResponse.rows) return log(`No secrets found for ${name}.`, "updateContract");
+    if (!nodeResponse.rows) return log(`No secrets found for ${dcName}.`, "updateContract");
     const rowBySlotId = nodeResponse.rows.find(row => row.slot_id === slotId);
-    if (!rowBySlotId) return log(`No secrets found for ${name} at slot ${slotId}.`, "updateContract");
+    if (!rowBySlotId) return log(`No secrets found for ${dcName} at slot ${slotId}.`, "updateContract");
     const { request: setDstConceroContractReq } = await publicClient.simulateContract({
-      address: contract,
+      address: dcContract,
       abi,
       functionName: "setDonHostedSecretsVersion",
       account,
       args: [rowBySlotId.version],
-      chain: viemChain,
+      chain: dcViemChain,
     });
     const setDstConceroContractHash = await walletClient.writeContract(setDstConceroContractReq);
     const { cumulativeGasUsed: setDstConceroContractGasUsed } = await publicClient.waitForTransactionReceipt({
       hash: setDstConceroContractHash,
     });
     log(
-      `Set ${name}:${contract} donHostedSecretsVersion[${name}, ${rowBySlotId.version}]. Gas used: ${setDstConceroContractGasUsed.toString()}`,
+      `Set ${dcName}:${dcContract} donHostedSecretsVersion[${rowBySlotId.version}]. Gas used: ${setDstConceroContractGasUsed.toString()}`,
       "setContractVariables",
     );
+
+    // Add Messenger to allowlist
+    try {
+      const { request: addToAllowlistReq } = await publicClient.simulateContract({
+        address: dcContract,
+        abi,
+        functionName: "setConceroMessenger",
+        account,
+        args: [process.env.MESSENGER_WALLET_ADDRESS],
+        chain: dcViemChain,
+      });
+      const addToAllowlistHash = await walletClient.writeContract(addToAllowlistReq);
+      const { cumulativeGasUsed: addToAllowlistGasUsed } = await publicClient.waitForTransactionReceipt({
+        hash: addToAllowlistHash,
+      });
+      log(
+        `Set ${dcName}:${dcContract} allowlist[${process.env.MESSENGER_WALLET_ADDRESS}]. Gas used: ${addToAllowlistGasUsed.toString()}`,
+        "setContractVariables",
+      );
+    } catch (error) {
+      if (error.message.includes("Address already in allowlist")) {
+        log(
+          `${process.env.MESSENGER_WALLET_ADDRESS} was already added to allowlist of ${dcContract}`,
+          "setContractVariables",
+        );
+      }
+    }
   }
 }

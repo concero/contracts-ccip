30cf30dd364b6e0ff4a675536a4e2d726b2d3bae
Oleg Kirillovich <olegkron@ya.ru>
2024-05-27 02:16:13 +0100

    deploy-infra

--- packages/hardhat/tasks/concero/deployInfra.ts
+++ packages/hardhat/tasks/concero/deployInfra.ts
@@ -10,23 +10,26 @@ import uploadDonSecrets from "../donSecrets/upload";
 import deployConcero from "../../deploy/02_Concero";
 import { execSync } from "child_process";
 
-export const liveChains: CNetwork[] = [chains.baseSepolia, chains.arbitrumSepolia];
+export const liveChains: CNetwork[] = [chains.baseSepolia, chains.arbitrumSepolia, chains.optimismSepolia];
 let deployableChains: CNetwork[] = liveChains;
 
 task("deploy-infra", "Deploy the CCIP infrastructure")
   .addFlag("skipdeploy", "Deploy the contract to a specific network")
+  // .addFlag("all", "Deploy the contract to all networks")
   .addOptionalParam("slotid", "DON-Hosted secrets slot id", 0, types.int)
   .setAction(async taskArgs => {
     const hre: HardhatRuntimeEnvironment = require("hardhat");
     const slotId = parseInt(taskArgs.slotid);
     const { name } = hre.network;
+
+    // if (taskArgs.all) deployableChains = liveChains;
+    // else
     if (name !== "localhost" && name !== "hardhat") deployableChains = [chains[name]];
 
-    if (!taskArgs.skipdeploy) {
+    if (taskArgs.skipdeploy) log("Skipping deployment", "deploy-infra");
+    else {
       execSync("yarn compile", { stdio: "inherit" });
       await deployConcero(hre, { slotId });
-    } else {
-      log("Skipping deployment", "deploy-infra");
     }
 
     await uploadDonSecrets(deployableChains, slotId, 4320);

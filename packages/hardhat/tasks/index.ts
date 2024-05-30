7a2b1bc6433a8b2060315888cd98d49d1efd9eea
lufaque <95128636+lufaque@users.noreply.github.com>
2024-05-30 17:54:59 +0300

    feat: set ConceroPool variables script

--- packages/hardhat/tasks/index.ts
+++ packages/hardhat/tasks/index.ts
@@ -1,19 +1,22 @@
 import billing from "./unused/Functions-billing";
 import consumer from "./unused/Functions-consumer";
+import clfRequest from "./unused/Functions-consumer";
 import simulate from "./script/simulate";
 import build from "./script/build";
 import deployCCIPInfrastructure from "./concero/deployInfra";
+import deployConceroPool from "./concero/deployConceroPool";
 import { fundContract } from "./concero/fundContract";
 import updateHashes from "./concero/updateHashes";
 import dripBnm from "./concero/dripBnm";
-import clfRequest from "./unused/Functions-consumer";
 import getHashSum from "./script/listHashes";
+
 export default {
   billing,
   consumer,
   simulate,
   build,
   deployCCIPInfrastructure,
+  deployConceroPool,
   fundContract,
   dripBnm,
   clfRequest,
--- packages/hardhat/utils/log.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function log(message, functionName) {
-    const greenFill = "\x1b[42m";
-    const reset = "\x1b[0m";
-    console.log(`${greenFill}[${functionName}]${reset}`, message);
-}
-exports.default = log;

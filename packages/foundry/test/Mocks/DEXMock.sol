9de5bf02e37e0edb64357cd0cb6a8d6e01103a26
Barba <patrickamc@protonmail.com>
2024-05-30 12:21:25 -0300

    Update Test and Adjusts

--- packages/foundry/test/Mocks/DEXMock.sol
+++ packages/foundry/test/Mocks/DEXMock.sol
@@ -41,16 +41,6 @@ contract DEXMock {
         emit DexMock_Transferred();
     }
 
-    //Sushi Single 
-    // function exactInputSingle(ISwapRouter.ExactInputSingleParams memory _params) external returns(uint256 amount){
-
-    //     IERC20(_params.tokenIn).safeTransferFrom(msg.sender, address(this), _params.amountIn);
-
-    //     IERC20(_params.tokenOut).safeTransfer(_params.recipient, _params.amountOutMinimum);
-
-    //     emit DexMock_Transferred();
-    // }
-
     //Uniswap Single
     function exactInputSingle(ISwapRouter02.ExactInputSingleParams memory _params) external returns(uint256 amount){
 
@@ -60,53 +50,6 @@ contract DEXMock {
 
         emit DexMock_Transferred();
     }
-
-
-    //Sushi & Uniswap Forks V3 Multi//
-    // function exactInput(ISwapRouter.ExactInputParams calldata _params) external payable returns (uint256 amountOut){
-
-    //     (address tokenIn, address tokenOut) = abi.decode(_params.path, (address,address));
-
-    //     IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), _params.amountIn);
-
-    //     IERC20(tokenOut).safeTransfer(_params.recipient, _params.amountOutMinimum);
-    //     emit DexMock_Transferred();
-    // }
-
-    //Uniswap Only V3 Multi//
-    // function exactInput(ISwapRouter02.ExactInputParams calldata _params) external payable returns(uint256 amountOut){
-    //     (address tokenIn, address tokenOut) = abi.decode(_params.path, (address,address));
-
-    //     IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), _params.amountIn);
-
-    //     IERC20(tokenOut).safeTransfer(_params.recipient, _params.amountOutMinimum);
-
-    //     emit DexMock_Transferred();
-    // }
-
-    //_swapDrome
-    // function swapExactTokensForTokens(
-    //     uint256 amountIn,
-    //     uint256 amountOutMin,
-    //     IRouter.Route[] calldata routes,
-    //     address to,
-    //     uint256 deadline
-    // ) external returns (uint[] memory amounts){
-
-    //         IRouter.Route[] memory route = new IRouter.Route[](routes.length);
-    //         route = routes;
-
-    //         IERC20(route[0].from).safeTransferFrom(msg.sender, address(this), amountIn);
-
-    //         if(route[0].to == USDC){
-    //             (amountIn * USDC_DECIMALS) / STANDARD_DECIMALS;
-
-    //             IERC20(USDC).safeTransfer(to, amountOutMin);
-    //         } else {
-    //             IERC20(route[0].to).safeTransfer(to, amountOutMin);
-    //             amountOutMin;
-    //         }
-    // }
 
     /// UTILITIES ///
 

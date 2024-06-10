e43ab6c2894572daa2e8ab6be1b9e9481865a0c0
lufaque <95128636+lufaque@users.noreply.github.com>
2024-06-10 19:56:36 +0300

    merge

--- packages/foundry/src/Interfaces/ISwapRouter02.sol
+++ packages/foundry/src/Interfaces/ISwapRouter02.sol
@@ -3,9 +3,9 @@ pragma solidity >=0.7.5;
 pragma abicoder v2;
 
 import {ISelfPermit} from "@uniswap/v3-periphery/contracts/interfaces/ISelfPermit.sol";
-import {IV2SwapRouter} from "@uniswap/swap-router-contracts/contracts/interfaces/IV2SwapRouter.sol";
-import {IV3SwapRouter} from "@uniswap/swap-router-contracts/contracts/interfaces/IV3SwapRouter.sol";
-import {IMulticallExtended} from "@uniswap/swap-router-contracts/contracts/interfaces/IMulticallExtended.sol";
+import {IV2SwapRouter} from "@uniswap/contracts/interfaces/IV2SwapRouter.sol";
+import {IV3SwapRouter} from "@uniswap/contracts/interfaces/IV3SwapRouter.sol";
+import {IMulticallExtended} from "@uniswap/contracts/interfaces/IMulticallExtended.sol";
 import "./solc_0.7.6/IApproveAndCall.sol";
 
 /// @title Router token swapping functionality

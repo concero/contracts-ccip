16339bb9ee64798fc7e567771e10fa1cd8ab2985
lufaque <95128636+lufaque@users.noreply.github.com>
2024-06-14 15:14:47 +0300

    fix: prettier

--- packages/hardhat/contracts/Orchestrator.sol
+++ packages/hardhat/contracts/Orchestrator.sol
@@ -9,6 +9,7 @@ import {IFunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/i
 import {IConcero} from "./Interfaces/IConcero.sol";
 import {IDexSwap} from "./Interfaces/IDexSwap.sol";
 import {Storage} from "./Libraries/Storage.sol";
+import "./Libraries/LibConcero.sol";
 
 ///////////////////////////////
 /////////////ERROR/////////////
@@ -25,170 +26,138 @@ error Orchestrator_InvalidAmount();
 error Orchestrator_OnlyRouterCanFulfill();
 
 contract Orchestrator is Storage, IFunctionsClient {
-  using SafeERC20 for IERC20;
+    using SafeERC20 for IERC20;
 
-  ///////////////
-  ///IMMUTABLE///
-  ///////////////
-  ///@notice the address of Functions router
-  address immutable i_router;
-  ///@notice variable to store the DexSwap address
-  address immutable i_dexSwap;
-  ///@notice variable to store the Concero address
-  address immutable i_concero;
-  ///@notice variable to store the ConceroPool address
-  address immutable i_pool;
-  ///@notice variable to store the immutable Proxy Address
-  address immutable i_proxy;
+    ///////////////
+    ///IMMUTABLE///
+    ///////////////
+    ///@notice the address of Functions router
+    address immutable i_router;
+    ///@notice variable to store the DexSwap address
+    address immutable i_dexSwap;
+    ///@notice variable to store the Concero address
+    address immutable i_concero;
+    ///@notice variable to store the ConceroPool address
+    address immutable i_pool;
+    ///@notice variable to store the immutable Proxy Address
+    address immutable i_proxy;
 
-  ////////////////////////////////////////////////////////
-  //////////////////////// EVENTS ////////////////////////
-  ////////////////////////////////////////////////////////
-  ///@notice emitted when the Functions router fulfills a request
-  event Orchestrator_RequestFulfilled(bytes32 requestId);
+    ////////////////////////////////////////////////////////
+    //////////////////////// EVENTS ////////////////////////
+    ////////////////////////////////////////////////////////
+    ///@notice emitted when the Functions router fulfills a request
+    event Orchestrator_RequestFulfilled(bytes32 requestId);
 
-  constructor(address _router, address _dexSwap, address _concero, address _pool, address _proxy) {
-    i_router = _router;
-    i_dexSwap = _dexSwap;
-    i_concero = _concero;
-    i_pool = _pool;
-    i_proxy = _proxy;
-  }
-
-  ///////////////
-  ///MODIFIERS///
-  ///////////////
-  modifier tokenAmountSufficiency(address token, uint256 amount) {
-    if (token != address(0)) {
-      uint256 balance = IERC20(token).balanceOf(msg.sender);
-      if (balance < amount) revert Orchestrator_InvalidAmount();
-    } else {
-      if (msg.value != amount) revert Orchestrator_InvalidAmount();
+    constructor(address _router, address _dexSwap, address _concero, address _pool, address _proxy) {
+        i_router = _router;
+        i_dexSwap = _dexSwap;
+        i_concero = _concero;
+        i_pool = _pool;
+        i_proxy = _proxy;
     }
-    _;
-  }
 
-  modifier validateBridgeData(BridgeData calldata _bridgeData) {
-    if (_bridgeData.amount == 0) {
-      revert Orchestrator_InvalidAmount();
+    ///////////////
+    ///MODIFIERS///
+    ///////////////
+    modifier tokenAmountSufficiency(address token, uint256 amount) {
+        if (token != address(0)) {
+            uint256 balance = IERC20(token).balanceOf(msg.sender);
+            if (balance < amount) revert Orchestrator_InvalidAmount();
+        } else {
+            if (msg.value != amount) revert Orchestrator_InvalidAmount();
+        }
+        _;
     }
-    _;
-  }
 
-  modifier validateSwapData(IDexSwap.SwapData[] calldata _srcSwapData) {
-    if (_srcSwapData.length == 0) {
-      revert IDexSwap.InvalidSwapData();
-    } 
-
-    if (_srcSwapData[0].fromToken == address(0)) {
-      if (_srcSwapData[0].fromAmount != msg.value) revert IDexSwap.InvalidSwapData();
+    modifier validateBridgeData(BridgeData calldata _bridgeData) {
+        if (_bridgeData.amount == 0) {
+            revert Orchestrator_InvalidAmount();
+        }
+        _;
     }
-    _;
-  }
 
-  ////////////////////
-  ///DELEGATE CALLS///
-  ////////////////////
-  function swapAndBridge(
-    BridgeData calldata _bridgeData,
-    IDexSwap.SwapData[] calldata _srcSwapData,
-    IDexSwap.SwapData[] calldata _dstSwapData
-  )
-    external
-    payable
-  {
-    uint256 amountToSwap = msg.value;
-    
-    _swap(_srcSwapData, amountToSwap);
+    modifier validateSwapData(IDexSwap.SwapData[] calldata _srcSwapData) {
+        if (_srcSwapData.length == 0) {
+            revert IDexSwap.InvalidSwapData();
+        }
 
-    (bool bridgeSuccess, bytes memory bridgeError) = i_concero.delegatecall(
-        abi.encodeWithSelector(
-            IConcero.startBridge.selector,
-            _bridgeData,
-            _dstSwapData
-        )
-    );
-    if(bridgeSuccess == false) revert Orchestrator_UnableToCompleteDelegateCall(bridgeError);
-  }
+        if (_srcSwapData[0].fromToken == address(0)) {
+            if (_srcSwapData[0].fromAmount != msg.value) revert IDexSwap.InvalidSwapData();
+        }
+        _;
+    }
 
-  //@audit adjust the modifier
-  function swap(IDexSwap.SwapData[] calldata _swapData) external payable tokenAmountSufficiency(_swapData[0].fromToken, _swapData[0].fromAmount)
-    validateSwapData(_swapData) {
+    ////////////////////
+    ///DELEGATE CALLS///
+    ////////////////////
+    function swapAndBridge(
+        BridgeData calldata _bridgeData,
+        IDexSwap.SwapData[] calldata _srcSwapData,
+        IDexSwap.SwapData[] calldata _dstSwapData
+    ) external payable {
+        uint256 amountToSwap = msg.value;
 
-    _swap(_swapData, msg.value);
-  }
+        _swap(_srcSwapData, amountToSwap);
 
-  function bridge(
-    BridgeData calldata bridgeData,
-    IDexSwap.SwapData[] calldata dstSwapData
-  ) external payable tokenAmountSufficiency(getToken(bridgeData.tokenType, s_chainIndex), bridgeData.amount) validateBridgeData(bridgeData) {
-
-    address fromToken = getToken(bridgeData.tokenType, s_chainIndex);
-
-    IERC20(fromToken).safeTransferFrom(msg.sender, address(this), bridgeData.amount);    
-    
-    (bool bridgeSuccess, bytes memory bridgeError) = i_concero.delegatecall(
-        abi.encodeWithSelector(
-            IConcero.startBridge.selector,
-            bridgeData,
-            dstSwapData
-        )
-    );
-    if(bridgeSuccess == false) revert Orchestrator_UnableToCompleteDelegateCall(bridgeError);
-  }
-  
-  //////////////////////////
-  /// INTERNAL FUNCTIONS ///
-  //////////////////////////
-  function _swap(IDexSwap.SwapData[] memory swapData, uint256 nativeAmount) internal returns (uint256 amountReceived) {
-    address fromToken = swapData[0].fromToken;
-    uint256 fromAmount = swapData[0].fromAmount;
-
-    if(fromToken != address(0)){
-        uint256 balanceBefore = IERC20(fromToken).balanceOf(address(this));
-        IERC20(fromToken).safeTransferFrom(msg.sender, address(this), fromAmount);
-        uint256 balanceAfter = IERC20(fromToken).balanceOf(address(this));
-
-        //TODO: deal with FoT tokens.
-        amountReceived = balanceAfter - balanceBefore;
-        if(amountReceived != fromAmount) revert Orchestrator_FoTNotAllowedYet();
-
-        (bool swapSuccess, bytes memory swapError) = i_dexSwap.delegatecall(
-            abi.encodeWithSelector(
-                IDexSwap.conceroEntry.selector,
-                fromAmount -= (fromAmount / CONCERO_FEE_FACTOR),
-                0
-            )
+        (bool bridgeSuccess, bytes memory bridgeError) = i_concero.delegatecall(
+            abi.encodeWithSelector(IConcero.startBridge.selector, _bridgeData, _dstSwapData)
         );
-        if(swapSuccess == false) revert Orchestrator_UnableToCompleteDelegateCall(swapError);
-    } else {
-        (bool swapSuccess, bytes memory swapError) = i_dexSwap.delegatecall(
-            abi.encodeWithSelector(
-                IDexSwap.conceroEntry.selector,
-                swapData,
-                nativeAmount -= (nativeAmount / CONCERO_FEE_FACTOR)
-            )
+        if (bridgeSuccess == false) revert Orchestrator_UnableToCompleteDelegateCall(bridgeError);
+    }
+
+    //@audit adjust the modifier
+    function swap(
+        IDexSwap.SwapData[] calldata _swapData
+    ) external payable tokenAmountSufficiency(_swapData[0].fromToken, _swapData[0].fromAmount) validateSwapData(_swapData) {
+        _swap(_swapData, msg.value);
+    }
+
+    function bridge(
+        BridgeData calldata bridgeData,
+        IDexSwap.SwapData[] calldata dstSwapData
+    ) external payable tokenAmountSufficiency(getToken(bridgeData.tokenType, s_chainIndex), bridgeData.amount) validateBridgeData(bridgeData) {
+        address fromToken = getToken(bridgeData.tokenType, s_chainIndex);
+
+        IERC20(fromToken).safeTransferFrom(msg.sender, address(this), bridgeData.amount);
+
+        (bool bridgeSuccess, bytes memory bridgeError) = i_concero.delegatecall(abi.encodeWithSelector(IConcero.startBridge.selector, bridgeData, dstSwapData));
+        if (bridgeSuccess == false) revert Orchestrator_UnableToCompleteDelegateCall(bridgeError);
+    }
+
+    //////////////////////////
+    /// INTERNAL FUNCTIONS ///
+    //////////////////////////
+    function _swap(IDexSwap.SwapData[] memory swapData, uint256 nativeAmount) internal returns (uint256 amountReceived) {
+        address fromToken = swapData[0].fromToken;
+        uint256 fromAmount = swapData[0].fromAmount;
+
+        if (fromToken != address(0)) {
+            //TODO: deal with FoT tokens.
+            LibConcero.transferFromERC20(fromToken, msg.sender, address(this), fromAmount);
+
+            (bool swapSuccess, bytes memory swapError) = i_dexSwap.delegatecall(
+                abi.encodeWithSelector(IDexSwap.conceroEntry.selector, fromAmount -= (fromAmount / CONCERO_FEE_FACTOR), 0)
+            );
+            if (swapSuccess == false) revert Orchestrator_UnableToCompleteDelegateCall(swapError);
+        } else {
+            (bool swapSuccess, bytes memory swapError) = i_dexSwap.delegatecall(
+                abi.encodeWithSelector(IDexSwap.conceroEntry.selector, swapData, nativeAmount -= (nativeAmount / CONCERO_FEE_FACTOR))
+            );
+            if (swapSuccess == false) revert Orchestrator_UnableToCompleteDelegateCall(swapError);
+        }
+    }
+
+    function handleOracleFulfillment(bytes32 requestId, bytes memory response, bytes memory err) external {
+        if (msg.sender != address(i_router)) {
+            revert Orchestrator_OnlyRouterCanFulfill();
+        }
+
+        (bool fulfilled, bytes memory notFulfilled) = i_concero.delegatecall(
+            abi.encodeWithSelector(IConcero.fulfillRequestWrapper.selector, requestId, response, err)
         );
-        if(swapSuccess == false) revert Orchestrator_UnableToCompleteDelegateCall(swapError);
+
+        if (fulfilled == false) revert Orchestrator_UnableToCompleteDelegateCall(notFulfilled);
+
+        emit Orchestrator_RequestFulfilled(requestId);
     }
-  }
-
-  function handleOracleFulfillment(bytes32 requestId, bytes memory response, bytes memory err) external {
-    if (msg.sender != address(i_router)) {
-      revert Orchestrator_OnlyRouterCanFulfill();
-    }
-
-    (bool fulfilled, bytes memory notFulfilled) = i_concero.delegatecall(
-      abi.encodeWithSelector(
-        IConcero.fulfillRequestWrapper.selector,
-        requestId,
-        response,
-        err
-      )
-    );
-
-    if(fulfilled == false) revert Orchestrator_UnableToCompleteDelegateCall(notFulfilled);
-
-    emit Orchestrator_RequestFulfilled(requestId);
-  }
 }
--- packages/hardhat/deploy/01_ConceroProxy.ts
+++ packages/hardhat/deploy/01_ConceroProxy.ts
@@ -3,17 +3,19 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { networkEnvKeys } from "../constants/CNetworks";
 import updateEnvVariable from "../utils/updateEnvVariable";
 import log from "../utils/log";
+import { getEnvVar } from "../utils/getEnvVar";
 
 const deployConceroProxy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { proxyDeployer } = await hre.getNamedAccounts();
 
   const { deploy } = hre.deployments;
   const { name } = hre.network;
+  const implementationAddress = getEnvVar(`CONCEROCCIP_${networkEnvKeys[name]}`);
 
   console.log("Deploying ConceroProxy...");
   const conceroProxyDeployment = (await deploy("ConceroProxy", {
     from: proxyDeployer,
-    args: ["0x3055cC530B8cF18fD996545EC025C4e677a1dAa3", proxyDeployer, "0x"],
+    args: [implementationAddress, proxyDeployer, "0x"],
     log: true,
     autoMine: true,
     gasLimit: 2_000_000,

7a587eb17381f4eaa9ee99338fabc2eaec2074fb
lufaque <95128636+lufaque@users.noreply.github.com>
2024-06-22 23:55:01 +0300

    fix: dst functions

--- packages/hardhat/tasks/CLFScripts/dist/DST.js
+++ packages/hardhat/tasks/CLFScripts/dist/DST.js
@@ -54,6 +54,32 @@ async function main() {
 				confirmations: 3n,
 				chainId: '0x13882',
 			},
+			[`0x${BigInt('4051577828743386545').toString(16)}`]: {
+				urls: [
+					`https://polygon-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`,
+					'https://polygon.blockpi.network/v1/rpc/public',
+					'https://polygon-bor-rpc.publicnode.com',
+				],
+				confirmations: 3n,
+				chainId: '0x89',
+			},
+			[`0x${BigInt('4949039107694359620').toString(16)}`]: {
+				urls: [
+					`https://arbitrum.infura.io/v3/${secrets.INFURA_API_KEY}`,
+					'https://arbitrum.blockpi.network/v1/rpc/public',
+					'https://arbitrum-rpc.publicnode.com',
+				],
+				confirmations: 3n,
+				chainId: '0xa4b1',
+			},
+			[`0x${BigInt('15971525489660198786').toString(16)}`]: {
+				urls: [
+					'https://base.blockpi.network/v1/rpc/public',
+					'https://base-rpc.publicnode.com',
+				],
+				confirmations: 3n,
+				chainId: '0x2105',
+			},
 		};
 		class FunctionsJsonRpcProvider extends ethers.JsonRpcProvider {
 			constructor(url) {

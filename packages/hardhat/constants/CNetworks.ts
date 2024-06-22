3e4a8696becef8a0a20954c63e564524892e482d
lufaque <95128636+lufaque@users.noreply.github.com>
2024-06-22 23:55:01 +0300

    fix: dst functions

--- packages/hardhat/constants/CNetworks.ts
+++ packages/hardhat/constants/CNetworks.ts
@@ -293,7 +293,8 @@ const CNetworks: Record<CNetworkNames, CNetwork> = {
   },
   polygon: {
     chainId: 137,
-    url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    // url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    url: `https://polygon-bor-rpc.publicnode.com`,
     accounts: [deployerPK, proxyDeployerPK],
     functionsDonId: process.env.CLF_DONID_POLYGON,
     functionsDonIdAlias: process.env.CLF_DONID_POLYGON_ALIAS,

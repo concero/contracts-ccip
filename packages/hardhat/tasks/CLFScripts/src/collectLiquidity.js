1dc5b353a8d852f6c577d87ac7edd4d5eb166a6d
lufaque <95128636+lufaque@users.noreply.github.com>
2024-06-22 12:42:07 +0300

    feat: collectLiquidity.js

---
+++ packages/hardhat/tasks/CLFScripts/src/collectLiquidity.js
@@ -0,0 +1,80 @@
+async function f() {
+	const ethers = await import('npm:ethers');
+
+	const chainSelectors = {
+		[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_FUJI}').toString(16)}`]: {
+			urls: [`https://avalanche-fuji.infura.io/v3/${secrets.INFURA_API_KEY}`],
+			chainId: '0xa869',
+			usdcAddress: '${USDC_FUJI}',
+			poolAddress: '${CONCEROPOOL_AVALANCHE_FUJI}',
+		},
+		[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_SEPOLIA}').toString(16)}`]: {
+			urls: [
+				`https://sepolia.infura.io/v3/${secrets.INFURA_API_KEY}`,
+				'https://ethereum-sepolia-rpc.publicnode.com',
+				'https://ethereum-sepolia.blockpi.network/v1/rpc/public',
+			],
+			chainId: '0xaa36a7',
+			usdcAddress: '${USDC_SEPOLIA}',
+			poolAddress: '${CONCEROPOOL_SEPOLIA}',
+		},
+		[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_ARBITRUM_SEPOLIA}').toString(16)}`]: {
+			urls: [
+				`https://arbitrum-sepolia.infura.io/v3/${secrets.INFURA_API_KEY}`,
+				'https://arbitrum-sepolia.blockpi.network/v1/rpc/public',
+				'https://arbitrum-sepolia-rpc.publicnode.com',
+			],
+			chainId: '0x66eee',
+			usdcAddress: '${USDC_ARBITRUM_SEPOLIA}',
+			poolAddress: '${CONCEROPOOL_ARBITRUM_SEPOLIA}',
+		},
+		[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_BASE_SEPOLIA}').toString(16)}`]: {
+			urls: [
+				`https://base-sepolia.g.alchemy.com/v2/${secrets.ALCHEMY_API_KEY}`,
+				'https://base-sepolia.blockpi.network/v1/rpc/public',
+				'https://base-sepolia-rpc.publicnode.com',
+			],
+			chainId: '0x14a34',
+			usdcAddress: '${USDC_BASE_SEPOLIA}',
+			poolAddress: '${CONCEROPOOL_BASE_SEPOLIA}',
+		},
+		[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_OPTIMISM_SEPOLIA}').toString(16)}`]: {
+			urls: [
+				`https://optimism-sepolia.infura.io/v3/${secrets.INFURA_API_KEY}`,
+				'https://optimism-sepolia.blockpi.network/v1/rpc/public',
+				'https://optimism-sepolia-rpc.publicnode.com',
+			],
+			chainId: '0xaa37dc',
+			usdcAddress: '${USDC_OPTIMISM_SEPOLIA}',
+			poolAddress: '${CONCEROPOOL_OPTIMISM_SEPOLIA}',
+		},
+		[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_POLYGON_AMOY}').toString(16)}`]: {
+			urls: [
+				`https://polygon-amoy.infura.io/v3/${secrets.INFURA_API_KEY}`,
+				'https://polygon-amoy.blockpi.network/v1/rpc/public',
+				'https://polygon-amoy-bor-rpc.publicnode.com',
+			],
+			chainId: '0x13882',
+			usdcAddress: '${USDC_AMOY}',
+			poolAddress: '${CONCEROPOOL_POLYGON_AMOY}',
+		},
+	};
+
+	class FunctionsJsonRpcProvider extends ethers.JsonRpcProvider {
+		constructor(url) {
+			super(url);
+			this.url = url;
+		}
+		async _send(payload) {
+			let resp = await fetch(this.url, {
+				method: 'POST',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify(payload),
+			});
+			const res = await resp.json();
+			if (res.length === undefined) return [res];
+			return res;
+		}
+	}
+}
+f();

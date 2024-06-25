6d535142715aab7bbe96571efbd3671176ccd3d3
lufaque <95128636+lufaque@users.noreply.github.com>
2024-06-25 19:16:10 +0300

    fix: src functions

--- packages/hardhat/constants/functionsJsCodeUrls.ts
+++ packages/hardhat/constants/functionsJsCodeUrls.ts
@@ -1,3 +1,3 @@
 export const srcJsCodeUrl = `https://raw.githubusercontent.com/concero/contracts-ccip/${process.env.CLF_JS_CODE_BRANCH}/packages/hardhat/tasks/CLFScripts/dist/SRC.min.js`;
 export const dstJsCodeUrl = `https://raw.githubusercontent.com/concero/contracts-ccip/${process.env.CLF_JS_CODE_BRANCH}/packages/hardhat/tasks/CLFScripts/dist/DST.min.js`;
-export const ethersV6CodeUrl = "https://raw.githubusercontent.com/ethers-io/ethers.js/v6.10.0/dist/ethers.umd.min.js";
+export const ethersV6CodeUrl = "https://raw.githubusercontent.com/ethers-io/ethers.js/v6.0.0/dist/ethers.umd.min.js";

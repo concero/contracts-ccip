ff3a6844e96abc3cc5ae5387421a8f819a2c091d
lufaque <95128636+lufaque@users.noreply.github.com>
2024-07-05 11:55:07 +0300

    fix: getTotalBalance.js

--- packages/hardhat/tasks/CLFScripts/src/pool/getTotalBalance.js
+++ packages/hardhat/tasks/CLFScripts/src/pool/getTotalBalance.js
@@ -24,7 +24,7 @@ async function f() {
 			],
 			chainId: '0x66eee',
 			usdcAddress: '${USDC_ARBITRUM_SEPOLIA}',
-			poolAddress: '${CHILD_POOL_ARBITRUM_SEPOLIA}',
+			poolAddress: '${CHILD_POOL_PROXY_ARBITRUM_SEPOLIA}',
 		},
 		[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_OPTIMISM_SEPOLIA}').toString(16)}`]: {
 			urls: [
@@ -34,7 +34,7 @@ async function f() {
 			],
 			chainId: '0xaa37dc',
 			usdcAddress: '${USDC_OPTIMISM_SEPOLIA}',
-			poolAddress: '${CHILD_POOL_OPTIMISM_SEPOLIA}',
+			poolAddress: '${CHILD_POOL_PROXY_OPTIMISM_SEPOLIA}',
 		},
 		// [`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_POLYGON_AMOY}').toString(16)}`]: {
 		// 	urls: [

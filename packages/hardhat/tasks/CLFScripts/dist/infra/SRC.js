69450cd571b3645d5d6ef5d8ba90c10d06599011
lufaque <95128636+lufaque@users.noreply.github.com>
2024-07-08 00:00:21 +0300

    feat: update pool js code

--- packages/hardhat/tasks/CLFScripts/dist/infra/SRC.js
+++ packages/hardhat/tasks/CLFScripts/dist/infra/SRC.js
@@ -198,32 +198,6 @@
 				resolve(undefined);
 			});
 		};
-		if (chainSelectors[chainSelector].priceFeed.maticUsd) {
-			const maticUsdContract = new ethers.Contract(
-				chainSelectors[chainSelector].priceFeed.maticUsd,
-				priceFeedsAbi,
-				provider,
-			);
-			promises.push(maticUsdContract.latestRoundData());
-		} else {
-			promises.push(promiseUndefined());
-		}
-		if (chainSelectors[chainSelector].priceFeed.ethUsd) {
-			const ethUsdContract = new ethers.Contract(chainSelectors[chainSelector].priceFeed.ethUsd, priceFeedsAbi, provider);
-			promises.push(ethUsdContract.latestRoundData());
-		} else {
-			promises.push(promiseUndefined());
-		}
-		if (chainSelectors[chainSelector].priceFeed.avaxUsd) {
-			const avaxUsdContract = new ethers.Contract(
-				chainSelectors[chainSelector].priceFeed.avaxUsd,
-				priceFeedsAbi,
-				provider,
-			);
-			promises.push(avaxUsdContract.latestRoundData());
-		} else {
-			promises.push(promiseUndefined());
-		}
 		const [linkUsd, usdcUsd, nativeUsd, linkNative, maticUsd, ethUsd, avaxUsd] = await Promise.all(promises);
 		return {
 			linkUsdc: linkUsd[1] > 0n ? (linkUsd[1] * 10n ** 18n) / usdcUsd[1] : 0n,
--- packages/hardhat/tasks/CLFScripts/src/infra/SRC.js
+++ packages/hardhat/tasks/CLFScripts/src/infra/SRC.js
@@ -210,32 +210,32 @@ numAllowedQueries: 2 – a minimum to initialise Viem.
 			});
 		};
 
-		if (chainSelectors[chainSelector].priceFeed.maticUsd) {
-			const maticUsdContract = new ethers.Contract(
-				chainSelectors[chainSelector].priceFeed.maticUsd,
-				priceFeedsAbi,
-				provider,
-			);
-			promises.push(maticUsdContract.latestRoundData());
-		} else {
-			promises.push(promiseUndefined());
-		}
-		if (chainSelectors[chainSelector].priceFeed.ethUsd) {
-			const ethUsdContract = new ethers.Contract(chainSelectors[chainSelector].priceFeed.ethUsd, priceFeedsAbi, provider);
-			promises.push(ethUsdContract.latestRoundData());
-		} else {
-			promises.push(promiseUndefined());
-		}
-		if (chainSelectors[chainSelector].priceFeed.avaxUsd) {
-			const avaxUsdContract = new ethers.Contract(
-				chainSelectors[chainSelector].priceFeed.avaxUsd,
-				priceFeedsAbi,
-				provider,
-			);
-			promises.push(avaxUsdContract.latestRoundData());
-		} else {
-			promises.push(promiseUndefined());
-		}
+		// if (chainSelectors[chainSelector].priceFeed.maticUsd) {
+		// 	const maticUsdContract = new ethers.Contract(
+		// 		chainSelectors[chainSelector].priceFeed.maticUsd,
+		// 		priceFeedsAbi,
+		// 		provider,
+		// 	);
+		// 	promises.push(maticUsdContract.latestRoundData());
+		// } else {
+		// 	promises.push(promiseUndefined());
+		// }
+		// if (chainSelectors[chainSelector].priceFeed.ethUsd) {
+		// 	const ethUsdContract = new ethers.Contract(chainSelectors[chainSelector].priceFeed.ethUsd, priceFeedsAbi, provider);
+		// 	promises.push(ethUsdContract.latestRoundData());
+		// } else {
+		// 	promises.push(promiseUndefined());
+		// }
+		// if (chainSelectors[chainSelector].priceFeed.avaxUsd) {
+		// 	const avaxUsdContract = new ethers.Contract(
+		// 		chainSelectors[chainSelector].priceFeed.avaxUsd,
+		// 		priceFeedsAbi,
+		// 		provider,
+		// 	);
+		// 	promises.push(avaxUsdContract.latestRoundData());
+		// } else {
+		// 	promises.push(promiseUndefined());
+		// }
 
 		const [linkUsd, usdcUsd, nativeUsd, linkNative, maticUsd, ethUsd, avaxUsd] = await Promise.all(promises);
 

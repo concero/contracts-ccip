3e589b98b54190ee46de8d54d8c0b5a4c8453efd
lufaque <95128636+lufaque@users.noreply.github.com>
2024-07-18 10:49:10 +0300

    feat: parent pool distribute liq js

--- packages/hardhat/tasks/CLFScripts/src/pool/distributeLiquidity.js
+++ packages/hardhat/tasks/CLFScripts/src/pool/distributeLiquidity.js
@@ -1,5 +1,19 @@
-(async () => {
+const ethers = await import('npm:ethers');
+
+// (async () => {
+try {
+	const [_, __, ___, newPoolChainSelector] = bytesArgs;
 	const chainSelectors = {
+		[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_ARBITRUM}').toString(16)}`]: {
+			urls: [
+				`https://arbitrum.infura.io/v3/${secrets.INFURA_API_KEY}`,
+				'https://arbitrum.blockpi.network/v1/rpc/public',
+				'https://arbitrum-rpc.publicnode.com',
+			],
+			chainId: '0xa4b1',
+			usdcAddress: '${USDC_ARBITRUM}',
+			poolAddress: '${CHILD_POOL_PROXY_ARBITRUM}',
+		},
 		[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_POLYGON}').toString(16)}`]: {
 			urls: [
 				`https://polygon-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`,
@@ -10,5 +24,93 @@
 			usdcAddress: '${USDC_POLYGON}',
 			poolAddress: '${CHILD_POOL_PROXY_POLYGON}',
 		},
+		[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_AVALANCHE}').toString(16)}`]: {
+			urls: [
+				`https://avalanche-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`,
+				'https://avalanche.blockpi.network/v1/rpc/public',
+				'https://avalanche-c-chain-rpc.publicnode.com',
+			],
+			chainId: '0xa86a',
+			usdcAddress: '${USDC_AVALANCHE}',
+			poolAddress: '${CHILD_POOL_PROXY_AVALANCHE}',
+		},
 	};
-})();
+
+	const erc20Abi = ['function balanceOf(address) external view returns (uint256)'];
+	const poolAbi = [
+		'function s_loansInUse() external view returns (uint256)',
+		'function distributeLiquidity(uint64, uint256) external',
+	];
+
+	class FunctionsJsonRpcProvider extends ethers.JsonRpcProvider {
+		constructor(url) {
+			super(url);
+			this.url = url;
+		}
+		async _send(payload) {
+			let resp = await fetch(this.url, {
+				method: 'POST',
+				headers: {'Content-Type': 'application/json'},
+				body: JSON.stringify(payload),
+			});
+			const res = await resp.json();
+			if (res.length === undefined) return [res];
+			return res;
+		}
+	}
+
+	const getPoolsBalances = async () => {
+		const getBalancePromises = [];
+
+		for (const chain in chainSelectors) {
+			if (chain === newPoolChainSelector) continue;
+			const url = chainSelectors[chain].urls[Math.floor(Math.random() * chainSelectors[chain].urls.length)];
+			const provider = new FunctionsJsonRpcProvider(url);
+			const erc20 = new ethers.Contract(chainSelectors[chain].usdcAddress, erc20Abi, provider);
+			const pool = new ethers.Contract(chainSelectors[chain].poolAddress, poolAbi, provider);
+			getBalancePromises.push(erc20.balanceOf(chainSelectors[chain].poolAddress));
+			getBalancePromises.push(pool.s_loansInUse());
+		}
+
+		const results = await Promise.all(getBalancePromises);
+		const balances = {};
+		const chainSelectorsArr = Object.keys(chainSelectors);
+
+		for (let i = 0, k = 0; i < results.length; i += 2, k++) {
+			balances[chainSelectorsArr[k]] = results[i] + results[i + 1];
+		}
+
+		return balances;
+	};
+
+	const poolsBalances = await getPoolsBalances();
+	console.log(poolsBalances);
+
+	// const poolsTotalBalance = poolsBalances.reduce((acc, pool) => acc + pool.balance, 0n);
+	// const poolsCount = Object.keys(chainSelectors).length;
+	// const newPoolBalance = poolsTotalBalance / BigInt(poolsCount);
+	// const distributeAmountPromises = [];
+	//
+	// for (const chain in chainSelectors) {
+	// 	if (chain === newPoolChainSelector) continue;
+	// 	const url = chainSelectors[chain].urls[Math.floor(Math.random() * chainSelectors[chain].urls.length)];
+	// 	const provider = new FunctionsJsonRpcProvider(url);
+	// 	const wallet = new ethers.Wallet('0x' + secrets.WALLET_PRIVATE_KEY, provider);
+	// 	const signer = wallet.connect(provider);
+	// 	const poolContract = new ethers.Contract(chainSelectors[chain].poolAddress, poolAbi, signer);
+	// 	const amountToDistribute = poolsBalances[chain] - newPoolBalance;
+	// 	console.log(amountToDistribute);
+	// 	// distributeAmountPromises.push(poolContract.distributeLiquidity(newPoolChainSelector, amountToDistribute));
+	// }
+
+	// await Promise.all(distributeAmountPromises);
+
+	return Functions.encodeUint256(1n);
+} catch (e) {
+	const {message, code} = e;
+	if (code === 'NONCE_EXPIRED' || message?.includes('replacement fee too low') || message?.includes('already known')) {
+		return Functions.encodeUint256(1n);
+	}
+	throw e;
+}
+// })();
--- packages/hardhat/tasks/script/simulate.ts
+++ packages/hardhat/tasks/script/simulate.ts
@@ -1,12 +1,17 @@
-import { task } from "hardhat/config";
+import { task, types } from "hardhat/config";
 import fs from "fs";
 import secrets from "../../constants/CLFSecrets";
 import CLFSimulationConfig from "../../constants/CLFSimulationConfig";
 import { execSync } from "child_process";
 import getHashSum from "../../utils/getHashSum";
-import { ethersV6CodeUrl, infraSrcJsCodeUrl } from "../../constants/functionsJsCodeUrls";
+import {
+  automationsJsCodeUrl,
+  ethersV6CodeUrl,
+  infraSrcJsCodeUrl,
+  parentPoolDistributeLiqJsCodeUrl,
+} from "../../constants/functionsJsCodeUrls";
 
-const { simulateScript, decodeResult } = require("@chainlink/functions-toolkit");
+const { simulateScript } = require("@chainlink/functions-toolkit");
 
 const path = require("path");
 const process = require("process");
@@ -52,80 +57,62 @@ async function simulate(pathToFile, args) {
 /* run with: bunx hardhat clf-simulate-script */
 task("clf-script-simulate", "Executes the JavaScript source code locally")
   // .addOptionalParam("path", "Path to script file", `${__dirn ame}/../Functions-request-config.js`, types.string)
+  .addParam("function", "Path to script file", "pool_get_total_balance", types.string)
   .setAction(async (taskArgs, hre) => {
     execSync(`bunx hardhat clf-script-build --all`, { stdio: "inherit" });
 
-    await simulate(path.join(__dirname, "../", "./CLFScripts/dist/infra/eval.min.js"), [
-      getHashSum(await (await fetch(infraSrcJsCodeUrl)).text()),
-      getHashSum(await (await fetch(ethersV6CodeUrl)).text()),
-      "0x0",
-      process.env.CONCERO_PROXY_POLYGON, // contractAddress
-      "0xf721b413e0a040abe87f48aff9801c78f037cab36cb43c72bd115ccec7845d27", // ccipMessageId
-      "0x70E73f067a1fC9FE6D53151bd271715811746d3a", // sender
-      "0x70E73f067a1fC9FE6D53151bd271715811746d3a", // recipient
-      "0x" + 100000000000000000n.toString(16), // amount
-      "0x" + BigInt(process.env.CL_CCIP_CHAIN_SELECTOR_POLYGON).toString(16), // srcChainSelector
-      "0x" + BigInt(process.env.CL_CCIP_CHAIN_SELECTOR_BASE).toString(16), // dstChainSelector
-      "0x" + 0n.toString(16), // token
-      "0xA65233", // blockNumber
-      "0xf721b413e0a040abe87f48aff9801c78f037cab36cb43c72bd115ccec7845d27",
-    ]);
-
-    // 0.252252162643023070
-    // 25.06559219644602
-
-    // BigInt('4051577828743386545') 4051577828743386545n
-    // srcChainSelector:  4051577828743386545n
-    // gasPrice:  30000000026n
-    // res:  30000000026n
-    //       30000000000
-    //       86000000000
-    //        1000000000
-    //      110000000000n
-
-    // await simulate(path.join(__dirname, "../", "./CLFScripts/dist/eval.min.js"), [
-    //   "0x1c5cf42d2b714c7f86edd64fb9b0c45b1fbd3dcac9831837d857f302833953e4", // srcJsHashSum
-    //   "0x05f8cc312ae3687e5581353da9c5889b92d232f7776c8b81dc234fb330fda265", // ethers hash sum
-    //   "0x0",
-    //   // process.env.CONCERO_BRIDGE_OPTIMISM_SEPOLIA, // contractAddress
-    //   "0x3055cC530B8cF18fD996545EC025C4e677a1dAa3", // contractAddress
-    //   "0x5315f93154194ca637615651c5662cf39a77308927ebe7d31c9e970958681a49", // ccipMessageId
-    //   "0x70E73f067a1fC9FE6D53151bd271715811746d3a", // sender
-    //   "0x70E73f067a1fC9FE6D53151bd271715811746d3a", // recipient
-    //   "0x" + 100000000000000000n.toString(16), // amount
-    //   "0x" + BigInt(process.env.CL_CCIP_CHAIN_SELECTOR_ARBITRUM_SEPOLIA).toString(16), // srcChainSelector
-    //   "0x" + BigInt(process.env.CL_CCIP_CHAIN_SELECTOR_OPTIMISM_SEPOLIA).toString(16), // dstChainSelector
-    //   "0x" + 0n.toString(16), // token
-    //   "0xA65233", // blockNumber
-    // ]);
-
-    // await simulate(path.join(__dirname, "../", "./CLFScripts/dist/eval.min.js"), [
-    //   "0xada5df165da01ec1249e7ae55303f8587fd50170729ed2b33a8b53be71f8d8ab",
-    //   "0x05f8cc312ae3687e5581353da9c5889b92d232f7776c8b81dc234fb330fda265", // ethers hash sum
-    //   "0x1",
-    //   process.env.CONCERO_BRIDGE_BASE_SEPOLIA, // srcContractAddress
-    //   "0x" + BigInt(process.env.CL_CCIP_CHAIN_SELECTOR_BASE_SEPOLIA).toString(16), // srcChainSelector, chain to get logs from
-    //   "0x92DA49", // blockNumber
-    //   // event params:
-    //   "0xc957703fb298a67ab8077f691dbf4cdb137be8fd39bd4afab67ef847f99a74c8", // messageId
-    //   "0x70E73f067a1fC9FE6D53151bd271715811746d3a", // sender
-    //   "0x70E73f067a1fC9FE6D53151bd271715811746d3a", // recipient
-    //   "0x" + 0n.toString(16), // token
-    //   "0x" + 40000000000000000n.toString(16), // amount
-    //   "0x" + 5224473277236331295n.toString(16), // dstChainSelector
-    // ]);
-
-    // await simulate(path.join(__dirname, "../", "./CLFScripts/dist/pool/parentPoolEval.min.js"), [
-    //   "0xef64cf53063700bbbd8e42b0282d3d8579aac289ea03f826cf16f9bd96c7703a", // srcJsHashSum
-    //   "0x984202f6c36a048a80e993557555488e5ae13ff86f2dfbcde698aacd0a7d4eb4", // ethers hash sum
-    // ]);
-
-    // await simulate(path.join(__dirname, "../", "./CLFScripts/dist/pool/automationEval.min.js"), [
-    //   getHashSum(await (await fetch(automationsJsCodeUrl)).text()),
-    //   getHashSum(await (await fetch(ethersV6CodeUrl)).text()),
-    //   "0xDddDDb8a8E41C194ac6542a0Ad7bA663A72741E0",
-    //   "0x186A0",
-    // ]);
+    if (taskArgs.function === "infra_src") {
+      await simulate(path.join(__dirname, "../", "./CLFScripts/dist/infra/eval.min.js"), [
+        getHashSum(await (await fetch(infraSrcJsCodeUrl)).text()),
+        getHashSum(await (await fetch(ethersV6CodeUrl)).text()),
+        "0x0",
+        process.env.CONCERO_PROXY_POLYGON, // contractAddress
+        "0xf721b413e0a040abe87f48aff9801c78f037cab36cb43c72bd115ccec7845d27", // ccipMessageId
+        "0x70E73f067a1fC9FE6D53151bd271715811746d3a", // sender
+        "0x70E73f067a1fC9FE6D53151bd271715811746d3a", // recipient
+        "0x" + 100000000000000000n.toString(16), // amount
+        "0x" + BigInt(process.env.CL_CCIP_CHAIN_SELECTOR_POLYGON).toString(16), // srcChainSelector
+        "0x" + BigInt(process.env.CL_CCIP_CHAIN_SELECTOR_BASE).toString(16), // dstChainSelector
+        "0x" + 0n.toString(16), // token
+        "0xA65233", // blockNumber
+        "0xf721b413e0a040abe87f48aff9801c78f037cab36cb43c72bd115ccec7845d27",
+      ]);
+    } else if (taskArgs.function === "infra_dst") {
+      await simulate(path.join(__dirname, "../", "./CLFScripts/dist/eval.min.js"), [
+        "0xada5df165da01ec1249e7ae55303f8587fd50170729ed2b33a8b53be71f8d8ab",
+        "0x05f8cc312ae3687e5581353da9c5889b92d232f7776c8b81dc234fb330fda265", // ethers hash sum
+        "0x1",
+        process.env.CONCERO_BRIDGE_BASE_SEPOLIA, // srcContractAddress
+        "0x" + BigInt(process.env.CL_CCIP_CHAIN_SELECTOR_BASE_SEPOLIA).toString(16), // srcChainSelector, chain to get logs from
+        "0x92DA49", // blockNumber
+        // event params:
+        "0xc957703fb298a67ab8077f691dbf4cdb137be8fd39bd4afab67ef847f99a74c8", // messageId
+        "0x70E73f067a1fC9FE6D53151bd271715811746d3a", // sender
+        "0x70E73f067a1fC9FE6D53151bd271715811746d3a", // recipient
+        "0x" + 0n.toString(16), // token
+        "0x" + 40000000000000000n.toString(16), // amount
+        "0x" + 5224473277236331295n.toString(16), // dstChainSelector
+      ]);
+    } else if (taskArgs.function === "pool_get_total_balance") {
+      await simulate(path.join(__dirname, "../", "./CLFScripts/dist/pool/parentPoolEval.min.js"), [
+        "0xef64cf53063700bbbd8e42b0282d3d8579aac289ea03f826cf16f9bd96c7703a", // srcJsHashSum
+        "0x984202f6c36a048a80e993557555488e5ae13ff86f2dfbcde698aacd0a7d4eb4", // ethers hash sum
+      ]);
+    } else if (taskArgs.function === "automation") {
+      await simulate(path.join(__dirname, "../", "./CLFScripts/dist/pool/automationEval.min.js"), [
+        getHashSum(await (await fetch(automationsJsCodeUrl)).text()),
+        getHashSum(await (await fetch(ethersV6CodeUrl)).text()),
+        "0xDddDDb8a8E41C194ac6542a0Ad7bA663A72741E0",
+        "0x186A0",
+      ]);
+    } else if (taskArgs.function === "pool_distribute_liq") {
+      await simulate(path.join(__dirname, "../", "./CLFScripts/dist/pool/distributeLiquidity.min.js"), [
+        getHashSum(await (await fetch(parentPoolDistributeLiqJsCodeUrl)).text()),
+        getHashSum(await (await fetch(ethersV6CodeUrl)).text()),
+        "0x1",
+        "0x383A1891AE1915B1",
+      ]);
+    }
   });
 
 export default {};

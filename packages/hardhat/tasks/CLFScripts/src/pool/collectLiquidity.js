608765b26b364997dfefc3299d33e72893543409
lufaque <95128636+lufaque@users.noreply.github.com>
2024-07-20 11:43:29 +0300

    fix: add fuji to js

--- packages/hardhat/tasks/CLFScripts/src/pool/collectLiquidity.js
+++ packages/hardhat/tasks/CLFScripts/src/pool/collectLiquidity.js
@@ -3,59 +3,69 @@ async function f() {
 		const [_, __, liquidityProvider, tokenAmount] = bytesArgs;
 
 		const chainSelectors = {
-			// [`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_ARBITRUM_SEPOLIA}').toString(16)}`]: {
-			// 	urls: [
-			// 		`https://arbitrum-sepolia.infura.io/v3/${secrets.INFURA_API_KEY}`,
-			// 		'https://arbitrum-sepolia.blockpi.network/v1/rpc/public',
-			// 		'https://arbitrum-sepolia-rpc.publicnode.com',
-			// 	],
-			// 	chainId: '0x66eee',
-			// 	usdcAddress: '${USDC_ARBITRUM_SEPOLIA}',
-			// 	poolAddress: '${CHILD_POOL_PROXY_ARBITRUM_SEPOLIA}',
-			// },
-			// [`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_OPTIMISM_SEPOLIA}').toString(16)}`]: {
-			// 	urls: [
-			// 		`https://optimism-sepolia.infura.io/v3/${secrets.INFURA_API_KEY}`,
-			// 		'https://optimism-sepolia.blockpi.network/v1/rpc/public',
-			// 		'https://optimism-sepolia-rpc.publicnode.com',
-			// 	],
-			// 	chainId: '0xaa37dc',
-			// 	usdcAddress: '${USDC_OPTIMISM_SEPOLIA}',
-			// 	poolAddress: '${CHILD_POOL_PROXY_OPTIMISM_SEPOLIA}',
-			// },
+			[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_ARBITRUM_SEPOLIA}').toString(16)}`]: {
+				urls: [
+					`https://arbitrum-sepolia.infura.io/v3/${secrets.INFURA_API_KEY}`,
+					'https://arbitrum-sepolia.blockpi.network/v1/rpc/public',
+					'https://arbitrum-sepolia-rpc.publicnode.com',
+				],
+				chainId: '0x66eee',
+				usdcAddress: '${USDC_ARBITRUM_SEPOLIA}',
+				poolAddress: '${CHILD_POOL_PROXY_ARBITRUM_SEPOLIA}',
+			},
+			[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_OPTIMISM_SEPOLIA}').toString(16)}`]: {
+				urls: [
+					`https://optimism-sepolia.infura.io/v3/${secrets.INFURA_API_KEY}`,
+					'https://optimism-sepolia.blockpi.network/v1/rpc/public',
+					'https://optimism-sepolia-rpc.publicnode.com',
+				],
+				chainId: '0xaa37dc',
+				usdcAddress: '${USDC_OPTIMISM_SEPOLIA}',
+				poolAddress: '${CHILD_POOL_PROXY_OPTIMISM_SEPOLIA}',
+			},
+			[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_FUJI}').toString(16)}`]: {
+				urls: [
+					`https://avalanche-fuji.infura.io/v3/${secrets.INFURA_API_KEY}`,
+					'https://avalanche-fuji-c-chain-rpc.publicnode.com',
+					'https://avalanche-fuji.blockpi.network/v1/rpc/public',
+				],
+				chainId: '0xa869',
+				usdcAddress: '${USDC_FUJI}',
+				poolAddress: '${CHILD_POOL_PROXY_FUJI}',
+			},
 
 			// mainnets
 
-			[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_ARBITRUM}').toString(16)}`]: {
-				urls: [
-					`https://arbitrum-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`,
-					'https://arbitrum.blockpi.network/v1/rpc/public',
-					'https://arbitrum-rpc.publicnode.com',
-				],
-				chainId: '0xa4b1',
-				usdcAddress: '${USDC_ARBITRUM}',
-				poolAddress: '${CHILD_POOL_PROXY_ARBITRUM}',
-			},
-			[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_POLYGON}').toString(16)}`]: {
-				urls: [
-					`https://polygon-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`,
-					'https://polygon.blockpi.network/v1/rpc/public',
-					'https://polygon-bor-rpc.publicnode.com',
-				],
-				chainId: '0x89',
-				usdcAddress: '${USDC_POLYGON}',
-				poolAddress: '${CHILD_POOL_PROXY_POLYGON}',
-			},
-			[`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_AVALANCHE}').toString(16)}`]: {
-				urls: [
-					`https://avalanche-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`,
-					'https://avalanche.blockpi.network/v1/rpc/public',
-					'https://avalanche-c-chain-rpc.publicnode.com',
-				],
-				chainId: '0xa86a',
-				usdcAddress: '${USDC_AVALANCHE}',
-				poolAddress: '${CHILD_POOL_PROXY_AVALANCHE}',
-			},
+			// [`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_ARBITRUM}').toString(16)}`]: {
+			// 	urls: [
+			// 		`https://arbitrum-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`,
+			// 		'https://arbitrum.blockpi.network/v1/rpc/public',
+			// 		'https://arbitrum-rpc.publicnode.com',
+			// 	],
+			// 	chainId: '0xa4b1',
+			// 	usdcAddress: '${USDC_ARBITRUM}',
+			// 	poolAddress: '${CHILD_POOL_PROXY_ARBITRUM}',
+			// },
+			// [`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_POLYGON}').toString(16)}`]: {
+			// 	urls: [
+			// 		`https://polygon-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`,
+			// 		'https://polygon.blockpi.network/v1/rpc/public',
+			// 		'https://polygon-bor-rpc.publicnode.com',
+			// 	],
+			// 	chainId: '0x89',
+			// 	usdcAddress: '${USDC_POLYGON}',
+			// 	poolAddress: '${CHILD_POOL_PROXY_POLYGON}',
+			// },
+			// [`0x${BigInt('${CL_CCIP_CHAIN_SELECTOR_AVALANCHE}').toString(16)}`]: {
+			// 	urls: [
+			// 		`https://avalanche-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`,
+			// 		'https://avalanche.blockpi.network/v1/rpc/public',
+			// 		'https://avalanche-c-chain-rpc.publicnode.com',
+			// 	],
+			// 	chainId: '0xa86a',
+			// 	usdcAddress: '${USDC_AVALANCHE}',
+			// 	poolAddress: '${CHILD_POOL_PROXY_AVALANCHE}',
+			// },
 		};
 
 		class FunctionsJsonRpcProvider extends ethers.JsonRpcProvider {

dee34a9defd7954216d63c4eb0f416a466822333
Oleg Kirillovich <olegkron@ya.ru>
2024-07-29 17:47:42 +0100

    add CLA to DeployParentPool.sol

--- packages/foundry/test/ParentPool/deploy/DeployParentPool.sol
+++ packages/foundry/test/ParentPool/deploy/DeployParentPool.sol
@@ -5,11 +5,15 @@ import {ParentPoolDeploy} from "../../../script/ParentPoolDeploy.s.sol";
 import {ParentPoolProxyDeploy} from "../../../script/ParentPoolProxyDeploy.s.sol";
 import {ConceroParentPool} from "contracts/ConceroParentPool.sol";
 import {ParentPoolProxy, ITransparentUpgradeableProxy} from "contracts/Proxy/ParentPoolProxy.sol";
+import {ConceroAutomation} from "contracts/ConceroAutomation.sol";
 import {Test, console} from "forge-std/Test.sol";
+import {LPToken} from "contracts/LPToken.sol";
 
 contract DeployParentPool is Test {
     ConceroParentPool public parentPoolImplementation;
     ParentPoolProxy public parentPoolProxy;
+    LPToken public lpToken;
+    ConceroAutomation public conceroCLA;
 
     function deployParentPool() internal {
         uint256 deployerPrivateKey = vm.envUint("FORGE_DEPLOYER_PRIVATE_KEY");
@@ -19,16 +23,28 @@ contract DeployParentPool is Test {
         address proxyDeployer = vm.envAddress("FORGE_PROXY_DEPLOYER");
 
         vm.selectFork(forkId);
-
         vm.startBroadcast(proxyDeployerPrivateKey);
+
         parentPoolProxy = new ParentPoolProxy(
             address(vm.envAddress("CONCERO_PAUSE_BASE")),
             proxyDeployer,
             bytes("")
         );
-        vm.stopBroadcast();
 
+        vm.stopBroadcast();
         vm.startBroadcast(deployerPrivateKey);
+
+        lpToken = new LPToken(deployer, address(parentPoolProxy));
+
+        conceroCLA = new ConceroAutomation(
+            vm.envBytes32("CLF_DONID_BASE"),
+            uint64(vm.envUint("CLF_SUBID_BASE_SEPOLIA")),
+            0,
+            vm.envAddress("CLF_ROUTER_BASE"),
+            address(parentPoolProxy),
+            address(deployer)
+        );
+
         parentPoolImplementation = new ConceroParentPool(
             address(parentPoolProxy),
             vm.envAddress("LINK_BASE"),
@@ -37,8 +53,8 @@ contract DeployParentPool is Test {
             address(vm.envAddress("CLF_ROUTER_BASE")),
             address(vm.envAddress("CL_CCIP_ROUTER_BASE")),
             address(vm.envAddress("USDC_BASE")),
-            address(vm.envAddress("LPTOKEN_BASE")),
-            address(vm.envAddress("CONCERO_AUTOMATION_BASE")),
+            address(lpToken),
+            address(conceroCLA),
             address(vm.envAddress("CONCERO_ORCHESTRATOR_BASE")),
             address(deployer)
         );

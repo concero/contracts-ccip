93e039321bf6af191a857caac346467ccd3e4d85
palmcivet7 <palmcivet@proton.me>
2024-07-29 21:28:05 +0100

    startWithdrawal unit test

--- packages/foundry/test/ParentPool/Withdraw.t.sol
+++ packages/foundry/test/ParentPool/Withdraw.t.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.20;
 
-import {Base_Test} from "./Base_Test.t.sol";
+import {Base_Test, console} from "./Base_Test.t.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract WithdrawTest is Base_Test {
@@ -34,7 +34,22 @@ contract WithdrawTest is Base_Test {
         vm.startPrank(liquidityProvider);
         IERC20(parentPoolImplementation.i_lp()).approve(address(parentPoolProxy), LP_BALANCE);
 
+        /// @dev call startWithdrawal via proxy
         (bool success,) = address(parentPoolProxy).call(abi.encodeWithSignature("startWithdrawal(uint256)", LP_BALANCE));
         require(success, "Function call failed");
+        vm.stopPrank();
+
+        /// @dev assert liquidityProvider no longer holds tokens
+        assertEq(IERC20(parentPoolImplementation.i_lp()).balanceOf(liquidityProvider), 0);
+
+        /// @dev use withdrawalId to view request params
+        bytes32 withdrawalId = parentPoolImplementation.getWithdrawalIdByLPAddress(liquidityProvider);
+
+        (address lpAddress, uint256 lpSupplySnapshot, uint256 lpAmountToBurn) =
+            parentPoolImplementation.getWithdrawRequestParams(withdrawalId);
+
+        console.log("lpAddress:", lpAddress);
+        console.log("lpSupplySnapshot:", lpSupplySnapshot);
+        console.log("lpAmountToBurn:", lpAmountToBurn);
     }
 }

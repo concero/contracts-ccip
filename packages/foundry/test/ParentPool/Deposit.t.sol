4e34daad976b899ddf54513ae4bff6cd60b1df43
Oleg Kirillovich <olegkron@ya.ru>
2024-07-30 01:19:17 +0100

    test_startDeposit_Success

--- packages/foundry/test/ParentPool/Deposit.t.sol
+++ packages/foundry/test/ParentPool/Deposit.t.sol
@@ -1,80 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {Test} from "../../lib/forge-std/src/Test.sol";
 import {DeployParentPool} from "./deploy/DeployParentPool.sol";
-import {console} from "../../lib/forge-std/src/console.sol";
-import {ConceroParentPool} from "contracts/ConceroParentPool.sol";
-import {IParentPool} from "contracts/Interfaces/IParentPool.sol";
-import {ParentPoolStorage} from "contracts/Libraries/ParentPoolStorage.sol";
+import {ParentPool_DepositWrapper, IDepositParentPool} from "./integrationTests/ParentPool_DepositWrapper.sol";
+import {Test, console, Vm} from "forge-std/Test.sol";
 
-contract Deposit is DeployParentPool, ConceroParentPool {
-    address public user1 = address(0x1);
-
+contract Deposit is DeployParentPool {
     function setUp() public {
-        deployPoolsInfra();
+        vm.selectFork(forkId);
+        deployParentPoolProxy();
+
+        ParentPool_DepositWrapper parentPool = new ParentPool_DepositWrapper(
+            address(parentPoolProxy),
+            vm.envAddress("LINK_BASE"),
+            vm.envBytes32("CLF_DONID_BASE"),
+            uint64(vm.envUint("CLF_SUBID_BASE")),
+            address(vm.envAddress("CLF_ROUTER_BASE")),
+            address(vm.envAddress("CL_CCIP_ROUTER_BASE")),
+            address(vm.envAddress("USDC_BASE")),
+            address(lpToken),
+            address(conceroCLA),
+            address(vm.envAddress("CONCERO_ORCHESTRATOR_BASE")),
+            address(deployer)
+        );
+
+        setProxyImplementation(address(parentPoolProxy), address(parentPool));
+        setParentPoolVars(address(parentPoolProxy), address(parentPool));
+        deployAutomation();
+        deployLpToken();
+        addFunctionsConsumer();
     }
 
     function test_startDeposit_Success() public {
         uint256 usdcAmount = 100 * 10 ** 6;
+        vm.recordLogs();
 
         vm.prank(user1);
-        IParentPool(address(parentPoolProxy)).startDeposit(usdcAmount);
+        IDepositParentPool(address(parentPoolProxy)).startDeposit(usdcAmount);
         vm.stopPrank();
+        // Get the emitted logs
+        Vm.Log[] memory entries = vm.getRecordedLogs();
 
-        // Add assertions to verify storage changes
-        // e.g., verify s_depositRequests, s_clfRequestTypes, emitted events, etc.
+        // Verify the emitted logs
+        assertEq(entries.length, 4);
+        bytes32 requestId = entries[0].topics[1];
 
-        bytes32 expectedRequestId = keccak256(
-            abi.encodePacked /* args to match the request ID calculation */()
-        );
+        //        // Verify storage changes using the emitted requestId
+        ParentPool_DepositWrapper.DepositRequest memory depositRequest = IDepositParentPool(
+            address(parentPoolProxy)
+        ).getDepositRequest(requestId);
 
-        // Verify storage changes
-        (
-            address lpAddress,
-            uint256 childPoolsLiquiditySnapshot,
-            uint256 usdcAmountToDeposit,
-            uint256 deadline
-        ) = IParentPool(address(parentPoolProxy)).s_depositRequests(expectedRequestId);
-        assertEq(lpAddress, user1);
-        assertEq(usdcAmountToDeposit, usdcAmount);
-        assertEq(deadline, block.timestamp + DEPOSIT_DEADLINE_SECONDS);
-
-        assertEq(
-            IParentPool(address(parentPoolProxy)).s_clfRequestTypes(expectedRequestId),
-            IParentPool(address(parentPoolProxy)).RequestType.startDeposit_getChildPoolsLiquidity
-        );
+        assertEq(depositRequest.lpAddress, address(user1));
+        assertEq(depositRequest.usdcAmountToDeposit, usdcAmount);
     }
-
-    function test_sendCcipTx() public {
-        deal(vm.envAddress("USDC_BASE"), address(parentPoolProxy), 10000000000000000000000);
-
-        vm.prank(vm.envAddress("MESSENGER_ADDRESS"));
-        IParentPool(address(parentPoolProxy)).distributeLiquidity(
-            uint64(vm.envUint("CL_CCIP_CHAIN_SELECTOR_ARBITRUM")),
-            10 * 10 ** 6,
-            keccak256(abi.encodePacked("test"))
-        );
-        vm.stopPrank();
-    }
-    //
-    //    function test_startDeposit_RevertAmountBelowMinimum() public {
-    //        uint256 usdcAmount = 10 * 10 ** 6; // Below minimum deposit
-    //
-    //        vm.prank(user1);
-    //        vm.expectRevert(
-    //            abi.encodeWithSelector(ConceroParentPool_AmountBelowMinimum.selector, MIN_DEPOSIT)
-    //        );
-    //        IParentPool(address(parentPoolProxy)).startDeposit(usdcAmount);
-    //    }
-    //
-    //    function test_startDeposit_RevertMaxCapReached() public {
-    //        uint256 usdcAmount = 1100 * 10 ** 6; // Above maximum deposit
-    //
-    //        vm.prank(user1);
-    //        vm.expectRevert(
-    //            abi.encodeWithSelector(ConceroParentPool_MaxCapReached.selector, MAX_DEPOSIT)
-    //        );
-    //        IParentPool(address(parentPoolProxy)).startDeposit(usdcAmount);
-    //    }
 }

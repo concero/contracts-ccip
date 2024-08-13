8dd78501572ff405ae7895b569540b63f9776341
Oleg Kirillovich <olegkron@ya.ru>
2024-08-13 20:11:24 +0100

    add functions to IOrchestrator.sol

--- packages/hardhat/contracts/Interfaces/IOrchestrator.sol
+++ packages/hardhat/contracts/Interfaces/IOrchestrator.sol
@@ -2,8 +2,26 @@
 pragma solidity 0.8.20;
 
 import {IStorage} from "./IStorage.sol";
+import "./IDexSwap.sol";
 
 interface IOrchestrator {
+
+    function swap(
+        IDexSwap.SwapData[] calldata _swapData,
+        address _receiver
+    ) external payable;
+
+    function bridge(
+        IStorage.BridgeData memory bridgeData,
+        IDexSwap.SwapData[] memory dstSwapData
+    ) external payable;
+
+    function swapAndBridge(
+        IStorage.BridgeData memory bridgeData,
+        IDexSwap.SwapData[] calldata srcSwapData,
+        IDexSwap.SwapData[] memory dstSwapData
+    ) external payable;
+
     function getTransaction(
         bytes32 _ccipMessageId
     ) external view returns (IStorage.Transaction memory transaction);

01994698d25f2ec27e577a4139b4ad618c939f46
lufaque <95128636+lufaque@users.noreply.github.com>
2024-08-20 01:08:21 +0300

    fix: update js code

--- packages/hardhat/tasks/CLFScripts/dist/pool/getTotalBalance.js
+++ packages/hardhat/tasks/CLFScripts/dist/pool/getTotalBalance.js
@@ -1,25 +1,31 @@
 (async () => {
 	const chainSelectors = {
-		['3478487238524512106']: {
-			urls: [`https://arbitrum-sepolia.infura.io/v3/${secrets.INFURA_API_KEY}`],
-			chainId: '0x66eee',
-			usdcAddress: '0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d',
-			poolAddress: '0x82F144741b9AD801FBb2fA52D3ee7B7e6e93B204',
+		['4949039107694359620']: {
+			urls: [`https://arbitrum-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`],
+			chainId: '0xa4b1',
+			usdcAddress: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831',
+			poolAddress: '0x164c20A4E11cBE0d8B5e23F5EE35675890BE280d',
 		},
-		['14767482510784806043']: {
-			urls: [`https://avalanche-fuji.infura.io/v3/${secrets.INFURA_API_KEY}`],
-			chainId: '0xa869',
-			usdcAddress: '0x5425890298aed601595a70ab815c96711a31bc65',
-			poolAddress: '0x3c69809aC32618F4E8842729b63A4679d1971aA5',
+		['4051577828743386545']: {
+			urls: [`https://polygon-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`],
+			chainId: '0x89',
+			usdcAddress: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359',
+			poolAddress: '0x164c20A4E11cBE0d8B5e23F5EE35675890BE280d',
 		},
-		['10344971235874465080']: {
-			urls: [`https://base-sepolia.g.alchemy.com/v2/${secrets.ALCHEMY_API_KEY}`],
-			chainId: '0x14a34',
-			usdcAddress: '0x036CbD53842c5426634e7929541eC2318f3dCF7e',
-			poolAddress: '0x5a42824F47257090A20894E18b3271ADbE6Ab228',
+		['6433500567565415381']: {
+			urls: [`https://avalanche-mainnet.infura.io/v3/${secrets.INFURA_API_KEY}`],
+			chainId: '0xa86a',
+			usdcAddress: '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E',
+			poolAddress: '0x164c20A4E11cBE0d8B5e23F5EE35675890BE280d',
+		},
+		['15971525489660198786']: {
+			urls: [`https://base-mainnet.g.alchemy.com/v2/${secrets.ALCHEMY_API_KEY}`],
+			chainId: '0x2105',
+			usdcAddress: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+			poolAddress: '0x0AE1B2730066AD46481ab0a5fd2B5893f8aBa323',
 		},
 	};
-	const baseChainSelector = '10344971235874465080';
+	const baseChainSelector = '15971525489660198786';
 	const erc20Abi = ['function balanceOf(address) external view returns (uint256)'];
 	const poolAbi = [
 		'function s_loansInUse() external view returns (uint256)',
@@ -59,7 +65,7 @@
 	};
 	const baseProvider = getProviderByChainSelector(baseChainSelector);
 	const getBaseDepositsOneTheWayArray = () => {
-		const pool = new ethers.Contract('0x5a42824F47257090A20894E18b3271ADbE6Ab228', poolAbi, baseProvider);
+		const pool = new ethers.Contract('0x0AE1B2730066AD46481ab0a5fd2B5893f8aBa323', poolAbi, baseProvider);
 		return pool.getDepositsOnTheWay();
 	};
 	const getChildPoolsCcipLogs = async ccipLines => {

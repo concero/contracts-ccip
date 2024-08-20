b20a1026bbb16aff75dd146410e0cffd7dbe45a5
Oleg Kirillovich <olegkron@ya.ru>
2024-08-20 02:56:33 +0100

    del

--- packages/hardhat/tasks/index.ts
+++ packages/hardhat/tasks/index.ts
@@ -1,9 +1,11 @@
 import * as concero from "./concero";
 import * as clf from "./CLF";
 import * as clfScripts from "./CLFScripts";
+import deleteDepositsOTWIds from "./concero/deleteDepositsOTWIds";
 
 export default {
   ...concero,
   ...clf,
   ...clfScripts,
+  deleteDepositsOTWIds,
 };

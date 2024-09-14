bceb0e41077fce84278bbd3eea51f2fc3300ff46
lufaque <95128636+lufaque@users.noreply.github.com>
2024-09-15 00:51:34 +0300

    fix: merge

--- packages/hardhat/tasks/concero/deployPool/deployChildPool.ts
+++ packages/hardhat/tasks/concero/deployPool/deployChildPool.ts
@@ -1,36 +1,32 @@
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import log from "../../../utils/log";
-import { execSync } from "child_process";
 import deployChildPool from "../../../deploy/08_ChildPool";
 import { setChildProxyVariables } from "./setChildProxyVariables";
 import deployProxyAdmin from "../../../deploy/10_ConceroProxyAdmin";
-import deployTransparentProxy, { ProxyType } from "../../../deploy/11_TransparentProxy";
+import deployTransparentProxy from "../../../deploy/11_TransparentProxy";
 import { upgradeProxyImplementation } from "../upgradeProxyImplementation";
+import { compileContracts } from "../../../utils/compileContracts";
 
 task("deploy-child-pool", "Deploy the pool")
-  .addFlag("skipdeploy", "Deploy the contract to a specific network")
   .addFlag("deployproxy", "Deploy the proxy")
-  .addFlag("skipsetvars", "Set the contract variables")
+  .addFlag("deployimplementation", "Deploy the implementation")
+  .addFlag("setvars", "Set the contract variables")
   .setAction(async taskArgs => {
     const hre: HardhatRuntimeEnvironment = require("hardhat");
-    const slotId = parseInt(taskArgs.slotid);
 
     if (taskArgs.deployproxy) {
       await deployProxyAdmin(hre, "childPoolProxy");
       await deployTransparentProxy(hre, "childPoolProxy");
     }
 
-    if (taskArgs.skipdeploy) {
-      log("Skipping deployment", "deploy-child-pool");
-    } else {
-      execSync("yarn compile", { stdio: "inherit" });
+    if (taskArgs.deployimplementation) {
+      compileContracts({ quiet: true });
 
       await deployChildPool(hre);
       await upgradeProxyImplementation(hre, ProxyType.childPool, false);
     }
 
-    if (!taskArgs.skipsetvars) {
+    if (taskArgs.setvars) {
       await setChildProxyVariables(hre);
     }
   });

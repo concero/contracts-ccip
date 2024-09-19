b8e789f487f3044e013a470c1ecb8618177cd8c2
lufaque <95128636+lufaque@users.noreply.github.com>
2024-09-19 16:26:34 +0300

    fix: parent pool deploy scripts

--- packages/hardhat/tasks/concero/deployPool/deployParentPool.ts
+++ packages/hardhat/tasks/concero/deployPool/deployParentPool.ts
@@ -14,6 +14,8 @@ import deployParentPool from "../../../deploy/ParentPool";
 import deployProxyAdmin from "../../../deploy/ConceroProxyAdmin";
 import { zeroAddress } from "viem";
 import { ProxyType } from "../../../constants/deploymentVariables";
+import deployParentPoolCLFCLA from "../../../deploy/ParenPoolCLFCLA";
+import { CLF_SECRETS_MAINNET_EXPIRATION, CLF_SECRETS_TESTNET_EXPIRATION } from "../../../constants/CLFSecretsConfig";
 
 task("deploy-parent-pool", "Deploy the pool")
   .addFlag("deployproxy", "Deploy the proxy")
@@ -29,6 +31,7 @@ task("deploy-parent-pool", "Deploy the pool")
     const slotId = parseInt(taskArgs.slotid);
     const { name } = hre.network;
     const deployableChains: CNetwork[] = [CNetworks[hre.network.name]];
+    const isTestnet = deployableChains[0].type === "testnet";
 
     if (taskArgs.deployproxy) {
       await deployProxyAdmin(hre, ProxyType.parentPoolProxy);
@@ -39,12 +42,18 @@ task("deploy-parent-pool", "Deploy the pool")
     }
 
     if (taskArgs.deployimplementation) {
-      await deployParentPool(hre, { automationForwarder: taskArgs.automationforwarder }); //todo: not passing slotId to deployParentPool functions' constructor args
+      await deployParentPoolCLFCLA(hre, { automationForwarder: taskArgs.automationforwarder });
+      await deployParentPool(hre, { automationForwarder: taskArgs.automationforwarder });
+
       await upgradeProxyImplementation(hre, ProxyType.parentPoolProxy, false);
     }
 
     if (taskArgs.uploadsecrets) {
-      await uploadDonSecrets(deployableChains, slotId, 4320);
+      await uploadDonSecrets(
+        deployableChains,
+        slotId,
+        isTestnet ? CLF_SECRETS_TESTNET_EXPIRATION : CLF_SECRETS_MAINNET_EXPIRATION,
+      );
     }
 
     if (taskArgs.setvars) {

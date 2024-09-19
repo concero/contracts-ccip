1741cc55fc86f45223ba5e9b40d88ea9d5c40349
lufaque <95128636+lufaque@users.noreply.github.com>
2024-09-19 17:07:10 +0300

    fix: remove interface

--- packages/hardhat/contracts/Interfaces/IParentPoolCCIP.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.20;
-
-interface IParentPoolCCIP {
-    function calculateLpAmount(
-        uint256 childPoolsBalance,
-        uint256 amountToDeposit
-    ) external view returns (uint256);
-}

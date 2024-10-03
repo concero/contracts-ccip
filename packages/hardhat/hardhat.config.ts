6dd771e92d21dfeb33c3b56e15a0ca09f30d02f6
Oleg <olegkron.cc@gmail.com>
2024-10-03 20:18:59 +0100

    Revert "explorer verifications & env"
    This reverts commit 427d827739095dc8b6153140733b4be3b72ea9e6.

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,6 +1,4 @@
 import "./utils/dotenvConfig";
-
-/* hardhat plugins */
 import "@nomicfoundation/hardhat-chai-matchers";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-viem";
@@ -10,18 +8,18 @@ import "hardhat-deploy";
 import "hardhat-deploy-ethers";
 import "hardhat-gas-reporter";
 import "hardhat-contract-sizer";
-import "@tenderly/hardhat-tenderly";
+import { HardhatUserConfig } from "hardhat/config";
 import "solidity-coverage";
 import "@chainlink/hardhat-chainlink";
-
 import cNetworks from "./constants/cNetworks";
-import { HardhatUserConfig } from "hardhat/config";
 import "./tasks";
-import { getEnvVar } from "./utils";
+import { setup as setupTenderly } from "@tenderly/hardhat-tenderly";
 
 // const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY ?? process.exit(1);
 // const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
 const enableGasReport = process.env.REPORT_GAS !== "false";
+
+setupTenderly();
 
 const config: HardhatUserConfig = {
   tenderly: {
@@ -57,28 +55,8 @@ const config: HardhatUserConfig = {
   etherscan: {
     apiKey: {
       avalancheFuji: "snowtrace",
-      arbitrum: getEnvVar("ARBISCAN_API_KEY"),
-      mainnet: getEnvVar("ETHERSCAN_API_KEY"),
-      polygon: getEnvVar("POLYGONSCAN_API_KEY"),
-      optimism: getEnvVar("OPTIMISM_API_KEY"),
     },
     customChains: [
-      {
-        network: "optimism",
-        chainId: 10,
-        urls: {
-          apiURL: "https://api-optimistic.etherscan.io/api",
-          browserURL: "https://optimistic.etherscan.io/",
-        },
-      },
-      {
-        network: "arbitrum",
-        chainId: 42161,
-        urls: {
-          apiURL: "https://api.arbiscan.io/api",
-          browserURL: "https://arbiscan.io/",
-        },
-      },
       {
         network: "avalancheFuji",
         chainId: 43114,

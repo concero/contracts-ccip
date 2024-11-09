a4d84f48841831cc454b8521e15f2aa3054d8e11
Oleg <olegkron.cc@gmail.com>
2024-11-09 11:38:42 +0000

    add getSwapAndBridge task

--- packages/hardhat/tasks/concero/getSwapAndBridgeCost/getCLFFeesTaken.ts
+++ packages/hardhat/tasks/concero/getSwapAndBridgeCost/getCLFFeesTaken.ts
@@ -10,42 +10,52 @@ export async function getCLFFeesTaken(
 ) {
   const clfFeeAbi = parseAbi(["function clfPremiumFees(uint64) external view returns(uint256)"]);
   const gasFeeAbi = parseAbi(["function s_lastGasPrices(uint64) external view returns(uint256)"]);
+  const ccipFeeAbi = parseAbi(["function s_latestLinkUsdcRate() external view returns(uint256)"]);
 
-  const [srcClfFeeTaken, dstClfFeeTaken, srcMessengerGasFeeTaken, dstMessengerGasFeeTaken] = await Promise.all([
-    publicClient.readContract({
-      address: infraProxyAddress,
-      abi: clfFeeAbi,
-      functionName: "clfPremiumFees",
-      args: [srcChainSelector],
-      blockNumber,
-    }),
-    publicClient.readContract({
-      address: infraProxyAddress,
-      abi: clfFeeAbi,
-      functionName: "clfPremiumFees",
-      args: [dstChainSelector],
-      blockNumber,
-    }),
-    publicClient.readContract({
-      address: infraProxyAddress,
-      abi: gasFeeAbi,
-      functionName: "s_lastGasPrices",
-      args: [srcChainSelector],
-      blockNumber,
-    }),
-    publicClient.readContract({
-      address: infraProxyAddress,
-      abi: gasFeeAbi,
-      functionName: "s_lastGasPrices",
-      args: [dstChainSelector],
-      blockNumber,
-    }),
-  ]);
+  const [srcClfFeeTaken, dstClfFeeTaken, srcMessengerGasFeeTaken, dstMessengerGasFeeTaken, latestLinkUsdcRate] =
+    await Promise.all([
+      publicClient.readContract({
+        address: infraProxyAddress,
+        abi: clfFeeAbi,
+        functionName: "clfPremiumFees",
+        args: [srcChainSelector],
+        blockNumber,
+      }),
+      publicClient.readContract({
+        address: infraProxyAddress,
+        abi: clfFeeAbi,
+        functionName: "clfPremiumFees",
+        args: [dstChainSelector],
+        blockNumber,
+      }),
+      publicClient.readContract({
+        address: infraProxyAddress,
+        abi: gasFeeAbi,
+        functionName: "s_lastGasPrices",
+        args: [srcChainSelector],
+        blockNumber,
+      }),
+      publicClient.readContract({
+        address: infraProxyAddress,
+        abi: gasFeeAbi,
+        functionName: "s_lastGasPrices",
+        args: [dstChainSelector],
+        blockNumber,
+      }),
+      publicClient.readContract({
+        address: infraProxyAddress,
+        abi: ccipFeeAbi,
+        functionName: "s_latestLinkUsdcRate",
+        args: [],
+        blockNumber,
+      }),
+    ]);
 
   return {
     srcClfFeeTaken,
     dstClfFeeTaken,
     srcMessengerGasFeeTaken,
     dstMessengerGasFeeTaken,
+    latestLinkUsdcRate,
   };
 }

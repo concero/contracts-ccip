cd9b68c7cd6c742193fd72339462a4e9ad4ab296
Oleg <olegkron.cc@gmail.com>
2024-11-26 19:53:23 +0000

    new deployments

--- tasks/concero/pools/setParentPoolVariables.ts
+++ tasks/concero/pools/setParentPoolVariables.ts
@@ -25,7 +25,6 @@ export async function setParentPoolJsHashes(chain: CNetwork, abi: any) {
     const collectLiquidityCode = await (await fetch(collectLiquidityCodeUrl)).text();
 
     const setHash = async (hash: string, functionName: string) => {
-      console.log("functionName:", functionName, hash);
       const setJsHashTxHash = await walletClient.writeContract({
         address: parentPoolProxy,
         abi,

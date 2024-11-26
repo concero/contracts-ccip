eccfdf14bcb6e48dd39d60f3d83a77497a55955e
Oleg <olegkron.cc@gmail.com>
2024-11-26 18:19:48 +0000

    rm var

--- constants/clfFees.ts
+++ constants/clfFees.ts
@@ -1,10 +1,10 @@
 import { CNetworkNames } from "../types/CNetwork";
 
 export const clfFees: Record<CNetworkNames, bigint> = {
-  polygon: 33131965864723535n,
+  polygon: 36000000000000000n,
   arbitrum: 20000000000000000n,
   base: 60000000000000000n,
-  avalanche: 240000000000000000n,
+  avalanche: 280000000000000000n,
 };
 
 export const defaultCLFfee = 20000000000000000n;

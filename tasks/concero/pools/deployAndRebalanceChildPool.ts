5a118d140a37f2ef4c111fed0145aaf74de2e3a3
Aleksei Gerasev <alexey@concero.io>
2024-11-26 23:36:13 +0300

    add task deployAndRebalanceChildPool

---
+++ tasks/concero/pools/deployAndRebalanceChildPool.ts
@@ -0,0 +1,38 @@
+import { task } from "hardhat/config";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import deployChildPool from "../../../deploy/ChildPool";
+import { setChildProxyVariables } from "./setChildProxyVariables";
+import deployProxyAdmin from "../../../deploy/ConceroProxyAdmin";
+import deployTransparentProxy from "../../../deploy/TransparentProxy";
+import { upgradeProxyImplementation } from "../upgradeProxyImplementation";
+import { compileContracts, verifyVariables } from "../../../utils";
+import { conceroNetworks, networkTypes, ProxyEnum } from "../../../constants";
+
+task("deploy-and-rebalance-child-pool", "Deploy and rebalance the child pool")
+    .addFlag("deployproxy", "Deploy the proxy")
+    .addFlag("deployimplementation", "Deploy the implementation")
+    .addFlag("setvars", "Set the contract variables")
+    .setAction(async taskArgs => {
+        const hre: HardhatRuntimeEnvironment = require("hardhat");
+        compileContracts({ quiet: true });
+        const networkType = conceroNetworks[hre.network.name].type;
+        if (networkType == networkTypes.mainnet) {
+            await verifyVariables();
+        }
+
+        if (taskArgs.deployproxy) {
+            await deployProxyAdmin(hre, ProxyEnum.childPoolProxy);
+            await deployTransparentProxy(hre, ProxyEnum.childPoolProxy);
+        }
+
+        if (taskArgs.deployimplementation) {
+            await deployChildPool(hre);
+            await upgradeProxyImplementation(hre, ProxyEnum.childPoolProxy, false);
+        }
+
+        if (taskArgs.setvars) {
+            await setChildProxyVariables(hre, true);
+        }
+    });
+
+export default {};

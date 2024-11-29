dd5b30916fc80a2928c652854849cdbf0c5b210f
Aleksei Gerasev <alexey@concero.io>
2024-11-29 22:18:10 +0300

    delete abicoder v2 because it already presents since 0.8.0

--- contracts/DexSwap.sol
+++ contracts/DexSwap.sol
@@ -5,7 +5,6 @@
  * @contact email: security@concero.io
  */
 pragma solidity 0.8.20;
-pragma abicoder v2;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";

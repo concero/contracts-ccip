d6c9035b0b57c9a83d5b6d34cbc03c60006005fc
lufaque <95128636+lufaque@users.noreply.github.com>
2024-12-15 17:26:32 +0300

    add cl env-enc support

--- tasks/concero/test.ts
+++ tasks/concero/test.ts
@@ -1,23 +1,11 @@
 import { task } from "hardhat/config";
 import conceroNetworks from "../../constants/conceroNetworks";
 
-function getHashSum(sourceCode: string) {
-  const hash = require("crypto").createHash("sha256");
-  hash.update(sourceCode, "utf8");
-  return hash.digest("hex");
-}
-
 task("test-script", "A test script").setAction(async taskArgs => {
   console.log(hre.network.name);
   const chain = conceroNetworks[hre.network.name];
 
   console.log("Running test-script");
-  // const [conceroProxy, conceroProxyAlias] = getEnvAddress("infraProxy", chain.name);
-  // console.log(conceroProxy, conceroProxyAlias);
-
-  // await deployProxyAdmin(hre, ProxyEnum.parentPool);
-  // await deployTransparentProxy(hre, ProxyEnum.parentPool);
-  // await upgradeProxyImplementation(hre, ProxyEnum.parentPool, false);
 });
 
 export default {};
